109bb4857bcfcfa4e7ea32acc11328a97d899207
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-04-08 21:02:06 +0200

    Fix setBlock flags Fix tile entity data being serialized twice Check if client data is null Add ClientUtils#getTextureManager and ClientUtils#translate

--- src/main/java/com/supermartijn642/core/block/BaseTileEntity.java
+++ src/main/java/com/supermartijn642/core/block/BaseTileEntity.java
@@ -21,7 +21,7 @@ public abstract class BaseTileEntity extends TileEntity {
     public void dataChanged(){
         this.dataChanged = true;
         this.markDirty();
-        this.world.notifyBlockUpdate(this.pos, this.getBlockState(), this.getBlockState(), 2 & 4);
+        this.world.notifyBlockUpdate(this.pos, this.getBlockState(), this.getBlockState(), 2 | 4);
     }
 
     protected abstract CompoundNBT writeData();
@@ -37,7 +37,7 @@ public abstract class BaseTileEntity extends TileEntity {
         super.write(compound);
         CompoundNBT data = this.writeData();
         if(data != null && !data.isEmpty())
-            compound.put("data", this.writeData());
+            compound.put("data", data);
         return compound;
     }
 
@@ -50,7 +50,9 @@ public abstract class BaseTileEntity extends TileEntity {
     @Override
     public CompoundNBT getUpdateTag(){
         CompoundNBT tag = super.write(new CompoundNBT());
-        tag.put("data", this.writeClientData());
+        CompoundNBT data = this.writeClientData();
+        if(data != null && !data.isEmpty())
+            tag.put("data", data);
         return tag;
     }
 
