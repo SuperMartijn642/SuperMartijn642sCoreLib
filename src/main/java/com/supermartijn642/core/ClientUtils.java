79bf7148cec7553f8cb7f063936da0b1db8d8d0c
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-04-08 21:00:07 +0200

    Fix setBlock flags Fix tile entity data being serialized twice Check if client data is null Add ClientUtils#getTextureManager and ClientUtils#translate

--- src/main/java/com/supermartijn642/core/ClientUtils.java
+++ src/main/java/com/supermartijn642/core/ClientUtils.java
@@ -1,6 +1,8 @@
 package com.supermartijn642.core;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.TextureManager;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.PlayerEntity;
 
 /**
@@ -10,6 +12,10 @@ public class ClientUtils {
 
     public static Minecraft getMinecraft(){
         return Minecraft.getInstance();
+    }
+
+    public static TextureManager getTextureManager(){
+        return getMinecraft().getTextureManager();
     }
 
     public static PlayerEntity getPlayer(){
@@ -22,6 +28,10 @@ public class ClientUtils {
 
     public static void queueTask(Runnable task){
         getMinecraft().enqueue(task);
+    }
+
+    public static String translate(String translationKey, Object... args){
+        return I18n.format(translationKey, args);
     }
 
 }
