195ac3d652aa5c75cf0080de713e29ef112dcba8
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-06-14 18:40:29 +0200

    Add BlockPos offset methods to RenderUtils

--- src/main/java/com/supermartijn642/core/render/RenderUtils.java
+++ src/main/java/com/supermartijn642/core/render/RenderUtils.java
@@ -2,20 +2,47 @@ package com.supermartijn642.core.render;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
 import com.supermartijn642.core.ClientUtils;
 import com.supermartijn642.core.block.BlockShape;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.RenderState;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.vector.Matrix4f;
 import net.minecraft.util.math.vector.Vector3d;
+import org.lwjgl.opengl.GL11;
+
+import java.util.OptionalDouble;
 
 /**
  * Created 6/12/2021 by SuperMartijn642
  */
 public class RenderUtils {
+
+    private static final RenderState.LayerState VIEW_OFFSET_Z_LAYERING = new RenderState.LayerState("view_offset_z_layering", () -> {
+        RenderSystem.pushMatrix();
+        RenderSystem.scalef(0.99975586F, 0.99975586F, 0.99975586F);
+    }, RenderSystem::popMatrix);
+    private static final RenderState.TransparencyState TRANSLUCENT_TRANSPARENCY = new RenderState.TransparencyState("translucent_transparency", () -> {
+        RenderSystem.enableBlend();
+        RenderSystem.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+    }, () -> {
+        RenderSystem.disableBlend();
+        RenderSystem.defaultBlendFunc();
+    });
+    private static final RenderType LINES_NO_DEPTH = RenderType.makeType(
+        "supermartijn642corelib:highlight",
+        DefaultVertexFormats.POSITION_COLOR,
+        GL11.GL_LINES,
+        128,
+        RenderType.State.getBuilder().line(new RenderState.LineState(OptionalDouble.of(1))).transparency(TRANSLUCENT_TRANSPARENCY).layer(VIEW_OFFSET_Z_LAYERING).writeMask(new RenderState.WriteMaskState(true, false)).depthTest(new RenderState.DepthTestState("no_depth", GL11.GL_ALWAYS)).build(false));
+    private static final IRenderTypeBuffer.Impl LINE_BUFFER = IRenderTypeBuffer.getImpl(new BufferBuilder(128));
 
     public static void enableDepthTest(){
         GlStateManager.enableDepthTest();
@@ -29,7 +56,7 @@ public class RenderUtils {
      * @return the current interpolated camera position.
      */
     public static Vector3d getCameraPosition(){
-        return ClientUtils.getMinecraft().gameRenderer.getActiveRenderInfo().getProjectedView();
+        return ClientUtils.getMinecraft().getRenderManager().info.getProjectedView();
     }
 
     /**
@@ -41,12 +68,13 @@ public class RenderUtils {
         Vector3d camera = getCameraPosition();
         matrixStack.translate(x - camera.x, y - camera.y, z - camera.z);
 
-        IVertexBuilder builder = ClientUtils.getMinecraft().getRenderTypeBuffers().getBufferSource().getBuffer(RenderType.getLines());
+        IVertexBuilder builder = LINE_BUFFER.getBuffer(LINES_NO_DEPTH);
         Matrix4f matrix4f = matrixStack.getLast().getMatrix();
         shape.forEachEdge((x1, y1, z1, x2, y2, z2) -> {
             builder.pos(matrix4f, (float)x1, (float)y1, (float)z1).color(red, green, blue, alpha).endVertex();
             builder.pos(matrix4f, (float)x2, (float)y2, (float)z2).color(red, green, blue, alpha).endVertex();
         });
+        LINE_BUFFER.finish();
 
         matrixStack.pop();
     }
