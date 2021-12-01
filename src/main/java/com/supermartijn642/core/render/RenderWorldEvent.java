bfab47273ec8bcd21d08042b7aee9da873a22f6b
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-12-01 14:35:52 +0100

    Add a RenderWorldEvent event using mixins

---
+++ src/main/java/com/supermartijn642/core/render/RenderWorldEvent.java
@@ -0,0 +1,21 @@
+package com.supermartijn642.core.render;
+
+import net.minecraftforge.eventbus.api.Event;
+
+/**
+ * Created 17/11/2021 by SuperMartijn642
+ * <p>
+ * Fired right after blocks are rendered and the {@link net.minecraftforge.client.event.DrawBlockHighlightEvent} is fired.
+ */
+public class RenderWorldEvent extends Event {
+
+    private final float partialTicks;
+
+    public RenderWorldEvent(float partialTicks){
+        this.partialTicks = partialTicks;
+    }
+
+    public float getPartialTicks(){
+        return this.partialTicks;
+    }
+}
--- src/test/java/com/supermartijn642/core/test/ClientProxy.java
+++ src/test/java/com/supermartijn642/core/test/ClientProxy.java
@@ -3,11 +3,11 @@ package com.supermartijn642.core.test;
 import com.mojang.blaze3d.platform.GlStateManager;
 import com.supermartijn642.core.block.BlockShape;
 import com.supermartijn642.core.render.RenderUtils;
+import com.supermartijn642.core.render.RenderWorldEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.math.Vec3d;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.DrawBlockHighlightEvent;
-import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -29,20 +29,20 @@ public class ClientProxy {
 //        GlStateManager.pushMatrix();
 //        GlStateManager.translated(-camera.x, -camera.y, -camera.z);
 //        RenderUtils.disableDepthTest();
-//        RenderUtils.renderShape(BlockShape.fullCube(), 1, 1, 0, 0.1f);
-//        RenderUtils.renderShapeSides(BlockShape.fullCube(), 0, 1, 1, 0.1f);
+//        RenderUtils.renderShape(BlockShape.fullCube(), 1, 1, 0, 0.5f);
+//        RenderUtils.renderShapeSides(BlockShape.fullCube(), 0, 1, 1, 0.5f);
 //        RenderUtils.resetState();
 //        GlStateManager.popMatrix();
     }
 
     @SubscribeEvent
-    public static void onRenderWorld(RenderWorldLastEvent e){
+    public static void onRenderWorld(RenderWorldEvent e){
         Vec3d camera = RenderUtils.getCameraPosition();
         GlStateManager.pushMatrix();
         GlStateManager.translated(-camera.x, -camera.y, -camera.z);
         RenderUtils.disableDepthTest();
-        RenderUtils.renderShape(BlockShape.fullCube(), 1, 1, 0, 0.1f);
-        RenderUtils.renderShapeSides(BlockShape.fullCube(), 0, 1, 1, 0.1f);
+        RenderUtils.renderShape(BlockShape.fullCube(), 1, 1, 0, 0.5f);
+        RenderUtils.renderShapeSides(BlockShape.fullCube(), 0, 1, 1, 0.5f);
         RenderUtils.resetState();
         GlStateManager.popMatrix();
     }
