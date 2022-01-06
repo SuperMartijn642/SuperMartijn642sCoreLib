5a7535c2bb6ce7568f46f6c732ecf4eb8421b5a2
SuperMartijn624 <martijnvandenbrand@outlook.com>
2022-01-06 22:57:03 +0100

    update version

--- src/main/java/com/supermartijn642/core/CoreLib.java
+++ src/main/java/com/supermartijn642/core/CoreLib.java
@@ -10,7 +10,7 @@ public class CoreLib {
 
     public static final String MODID = "supermartijn642corelib";
     public static final String NAME = "SuperMartijn642's Core Lib";
-    public static final String VERSION = "1.0.15a";
+    public static final String VERSION = "1.0.16";
 
     public CoreLib(){
     }
