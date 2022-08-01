c787cff6465517e0c1470be5a73687adf112d499
SuperMartijn624 <martijnvandenbrand@outlook.com>
2022-08-01 13:46:10 +0200

    Use tags for harvest tool and harvest level

--- src/main/java/com/supermartijn642/core/mixin/ForgeHooksMixin.java
+++ src/main/java/com/supermartijn642/core/mixin/ForgeHooksMixin.java
@@ -3,15 +3,18 @@ package com.supermartijn642.core.mixin;
 import com.supermartijn642.core.block.BaseBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorldReader;
 import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.common.ToolType;
 import net.minecraftforge.event.ForgeEventFactory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
@@ -47,6 +50,53 @@ public class ForgeHooksMixin {
             }
 
             ci.setReturnValue(ForgeEventFactory.doPlayerHarvestCheck(player, state, toolLevel >= state.getHarvestLevel()));
+        }
+    }
+
+    @Redirect(
+        method = "canHarvestBlock",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/item/Item;getHarvestLevel(Lnet/minecraft/item/ItemStack;Lnet/minecraftforge/common/ToolType;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/block/BlockState;)I"
+        ),
+        remap = false
+    )
+    private static int canHarvestBlockRedirect(Item item, ItemStack stack, ToolType type, PlayerEntity player, BlockState state){
+        if(state.getBlock() instanceof BaseBlock){
+            int bestHarvestLevel = -1;
+            for(ToolType toolType : stack.getToolTypes()){
+                if(state.isToolEffective(toolType)){
+                    int harvestLevel = stack.getHarvestLevel(toolType, player, state);
+                    if(harvestLevel > bestHarvestLevel)
+                        bestHarvestLevel = harvestLevel;
+                }
+            }
+            if(bestHarvestLevel == -1)
+                bestHarvestLevel = stack.getHarvestLevel(type, player, state);
+            return bestHarvestLevel;
+        }
+        return item.getHarvestLevel(stack, type, player, state);
+    }
+
+    @Inject(
+        method = "canToolHarvestBlock",
+        at = @At("HEAD"),
+        cancellable = true,
+        remap = false
+    )
+    private static void canToolHarvestBlock(IWorldReader world, BlockPos pos, ItemStack stack, CallbackInfoReturnable<Boolean> ci){
+        if(stack.isEmpty())
+            return;
+        BlockState state = world.getBlockState(pos);
+        int harvestLevel = state.getHarvestLevel();
+        if(state.getBlock() instanceof BaseBlock){
+            for(ToolType toolType : stack.getToolTypes()){
+                if(state.isToolEffective(toolType) && stack.getHarvestLevel(toolType, null, state) >= harvestLevel){
+                    ci.setReturnValue(true);
+                    return;
+                }
+            }
+            ci.setReturnValue(false);
         }
     }
 }
