72873cd1ef27749c96b82ae7a26ba21c4c4b7fae
SuperMartijn624 <martijnvandenbrand@outlook.com>
2022-09-01 20:10:54 +0200

    Add recipe condition serializers in RegistryEntryAcceptor.Registry

--- src/main/java/com/supermartijn642/core/registry/RegistryEntryAcceptor.java
+++ src/main/java/com/supermartijn642/core/registry/RegistryEntryAcceptor.java
@@ -49,7 +49,8 @@ public @interface RegistryEntryAcceptor {
         PAINTING_VARIANTS(Registries.PAINTING_VARIANTS),
         RECIPE_SERIALIZERS(Registries.RECIPE_SERIALIZERS),
         ATTRIBUTES(Registries.ATTRIBUTES),
-        STAT_TYPES(Registries.STAT_TYPES);
+        STAT_TYPES(Registries.STAT_TYPES),
+        RECIPE_CONDITION_SERIALIZERS(Registries.RECIPE_CONDITION_SERIALIZERS);
 
         public final Registries.Registry<?> registry;
 
