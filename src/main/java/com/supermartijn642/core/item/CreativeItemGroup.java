4862130b4b9191a4641c424a404c48a0ae595d2b
SuperMartijn624 <martijnvandenbrand@outlook.com>
2022-09-09 22:04:50 +0200

    Add CreativeItemGroup#filler and CreativeItemGroup#sorter

--- src/main/java/com/supermartijn642/core/item/CreativeItemGroup.java
+++ src/main/java/com/supermartijn642/core/item/CreativeItemGroup.java
@@ -1,10 +1,14 @@
 package com.supermartijn642.core.item;
 
+import com.supermartijn642.core.TextComponents;
 import com.supermartijn642.core.registry.RegistryUtil;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IItemProvider;
+import net.minecraft.util.NonNullList;
 
+import java.util.Comparator;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 /**
@@ -82,12 +86,39 @@ public class CreativeItemGroup extends ItemGroup {
     private final String identifier;
     private final String translationKey;
     private final Supplier<ItemStack> icon;
+    private Consumer<Consumer<ItemStack>> filler;
+    private Comparator<ItemStack> sorter;
 
     private CreativeItemGroup(String identifier, String translationKey, Supplier<ItemStack> icon){
         super(identifier);
         this.identifier = identifier;
         this.translationKey = translationKey;
         this.icon = icon;
+    }
+
+    /**
+     * Sets a custom filler for this creative tab. By default, the creative will be filled by items with this tab set in their properties.
+     * @param filler a functions which pushes items to the given consumer
+     */
+    public CreativeItemGroup filler(Consumer<Consumer<ItemStack>> filler){
+        this.filler = filler;
+        return this;
+    }
+
+    /**
+     * Sets a sorter for the items in this creative tab.
+     * @param sorter compares two item stacks
+     */
+    public CreativeItemGroup sorter(Comparator<ItemStack> sorter){
+        this.sorter = sorter;
+        return this;
+    }
+
+    /**
+     * Set the sorter to sort items alphabetically based on their display name.
+     */
+    public CreativeItemGroup sortAlphabetically(){
+        return this.sorter(Comparator.comparing(stack -> TextComponents.itemStack(stack).format()));
     }
 
     @Override
@@ -111,5 +142,17 @@ public class CreativeItemGroup extends ItemGroup {
     @Override
     public String getRecipeFolderName(){
         return this.identifier;
+    }
+
+    @Override
+    public void fillItemList(NonNullList<ItemStack> items){
+        // Fill the list with items
+        if(this.filler == null)
+            super.fillItemList(items);
+        else
+            this.filler.accept(items::add);
+        // Sort the items
+        if(this.sorter != null)
+            items.sort(this.sorter);
     }
 }
