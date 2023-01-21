627f098dba5650c58f3ebf2e74075fc136daf057
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-01-21 01:09:44 +0100

    Fix atlas sprite registration through ClientRegistrationHandler

--- src/main/java/com/supermartijn642/core/mixin/SpriteResourceLoaderMixin.java
+++ src/main/java/com/supermartijn642/core/mixin/SpriteResourceLoaderMixin.java
@@ -1,7 +1,9 @@
 package com.supermartijn642.core.mixin;
 
+import com.supermartijn642.core.registry.ClientRegistrationHandler;
 import net.minecraft.client.renderer.texture.atlas.SpriteResourceLoader;
 import net.minecraft.client.renderer.texture.atlas.SpriteSource;
+import net.minecraft.client.renderer.texture.atlas.sources.SingleFile;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,6 +11,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created 01/01/2023 by SuperMartijn642
@@ -21,7 +24,8 @@ public class SpriteResourceLoaderMixin {
         at = @At("STORE"),
         ordinal = 0
     )
-    public List<SpriteSource> appendSprites(List<SpriteSource> sprites, ResourceManager resourceManager, ResourceLocation atlas){
+    private static List<SpriteSource> appendSprites(List<SpriteSource> sprites, ResourceManager resourceManager, ResourceLocation atlas){
+        ClientRegistrationHandler.collectSprites(atlas, location -> sprites.add(new SingleFile(location, Optional.empty())));
         return sprites;
     }
 }
