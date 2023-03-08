5574b14aa17cd22f118403a97bf422efe188b342
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-03-08 20:25:28 +0100

    Fix model overwrites not being applied correctly

--- src/main/java/com/supermartijn642/core/mixin/ModelManagerMixin.java
+++ src/main/java/com/supermartijn642/core/mixin/ModelManagerMixin.java
@@ -2,12 +2,17 @@ package com.supermartijn642.core.mixin;
 
 import com.supermartijn642.core.CoreLib;
 import com.supermartijn642.core.registry.ClientRegistrationHandler;
+import net.minecraft.client.resources.model.AtlasSet;
+import net.minecraft.client.resources.model.ModelBakery;
 import net.minecraft.client.resources.model.ModelManager;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.profiling.ProfilerFiller;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Map;
 
 /**
  * Created 27/07/2022 by SuperMartijn642
@@ -18,16 +23,16 @@ public class ModelManagerMixin {
     @Inject(
         at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/util/profiling/ProfilerFiller;popPush(Ljava/lang/String;)V",
+            target = "Lnet/minecraft/client/resources/model/ModelBakery;getBakedTopLevelModels()Ljava/util/Map;",
             ordinal = 0,
             shift = At.Shift.BEFORE
         ),
-        method = "apply"
+        method = "loadModels"
     )
-    private void apply(ModelManager.ReloadState reloadState, ProfilerFiller profilerFiller, CallbackInfo ci){
+    private void loadModels(ProfilerFiller profilerFiller, Map<ResourceLocation,AtlasSet.StitchResult> map, ModelBakery modelBakery, CallbackInfoReturnable<ModelManager.ReloadState> ci){
         // Catch errors here to prevent the model manager from continuously retrying to load models
         try{
-            ClientRegistrationHandler.registerModelOverwritesInternal(reloadState.modelBakery().getBakedTopLevelModels());
+            ClientRegistrationHandler.registerModelOverwritesInternal(modelBakery.getBakedTopLevelModels());
         }catch(Exception e){
             CoreLib.LOGGER.error("Encountered an error while applying model overwrites!", e);
         }
