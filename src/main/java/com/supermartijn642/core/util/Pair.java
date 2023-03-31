aeded6e946b0b7c096bf112e40e633022df0fd0c
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-03-31 18:05:10 +0200

    Improve Either, Maybe, and Pair

--- src/main/java/com/supermartijn642/core/util/Pair.java
+++ src/main/java/com/supermartijn642/core/util/Pair.java
@@ -2,12 +2,16 @@ package com.supermartijn642.core.util;
 
 import com.google.common.base.Objects;
 
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
 /**
  * Created 23/07/2022 by SuperMartijn642
  */
 public class Pair<X, Y> {
 
-    public static <X,Y> Pair<X,Y> of(X left, Y right){
+    public static <X, Y> Pair<X,Y> of(X left, Y right){
         return new Pair<>(left, right);
     }
 
@@ -25,6 +29,41 @@ public class Pair<X, Y> {
 
     public Y right(){
         return this.right;
+    }
+
+    /**
+     * Applies the given mapper to the left value.
+     */
+    public <S> Pair<S,Y> mapLeft(Function<X,S> mapper){
+        return Pair.of(mapper.apply(this.left), this.right);
+    }
+
+    /**
+     * Applies the given mapper to the right value.
+     */
+    public <S> Pair<X,S> mapRight(Function<Y,S> mapper){
+        return Pair.of(this.left, mapper.apply(this.right));
+    }
+
+    /**
+     * Applies the respective mapper to the left or right values.
+     */
+    public <R, S> Pair<R,S> map(Function<X,R> mapLeft, Function<Y,S> mapRight){
+        return Pair.of(mapLeft.apply(this.left), mapRight.apply(this.right));
+    }
+
+    /**
+     * Maps the values held by this pair to an object of type {@link S}.
+     */
+    public <S> S flatMap(BiFunction<X,Y,S> mapper){
+        return mapper.apply(this.left, this.right);
+    }
+
+    /**
+     * Applies the given consumer to the values held by this pair.
+     */
+    public void apply(BiConsumer<X,Y> consumer){
+        consumer.accept(this.left, this.right);
     }
 
     @Override
