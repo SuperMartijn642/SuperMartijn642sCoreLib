31bebcf952c3ecb8265a08a179cbb4314a214489
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-05-07 03:17:27 +0200

    Rework data generator processing and add ResourceAggregator

---
+++ src/main/java/com/supermartijn642/core/mixin/DatagenModLoaderAccessor.java
@@ -0,0 +1,24 @@
+package com.supermartijn642.core.mixin;
+
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.data.event.GatherDataEvent;
+import net.minecraftforge.data.loading.DatagenModLoader;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+/**
+ * Created 07/05/2023 by SuperMartijn642
+ */
+@Mixin(value = DatagenModLoader.class, remap = false)
+public interface DatagenModLoaderAccessor {
+
+    @Accessor(value = "dataGeneratorConfig", remap = false)
+    static GatherDataEvent.DataGeneratorConfig getDataGeneratorConfig(){
+        throw new AssertionError();
+    }
+
+    @Accessor(value = "existingFileHelper", remap = false)
+    static ExistingFileHelper getExistingFileHelper(){
+        throw new AssertionError();
+    }
+}
