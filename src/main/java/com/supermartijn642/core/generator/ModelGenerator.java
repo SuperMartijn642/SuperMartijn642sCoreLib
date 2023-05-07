a59c7a9336980d747106d3e1163ba6d36e12574e
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-05-07 03:12:14 +0200

    Use LinkedHashMap instead of HashMap to guarantee json files are always generated the same

--- src/main/java/com/supermartijn642/core/generator/ModelGenerator.java
+++ src/main/java/com/supermartijn642/core/generator/ModelGenerator.java
@@ -12,10 +12,7 @@ import net.minecraft.util.Direction;
 import net.minecraft.util.IItemProvider;
 import net.minecraft.util.ResourceLocation;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
@@ -453,8 +450,8 @@ public abstract class ModelGenerator extends ResourceGenerator {
 
         protected final String modid;
         protected final ResourceLocation identifier;
-        private final Map<String,String> textures = new HashMap<>();
-        private final Map<ItemCameraTransforms.TransformType,TransformBuilder> transforms = new HashMap<>();
+        private final Map<String,String> textures = new LinkedHashMap<>();
+        private final Map<ItemCameraTransforms.TransformType,TransformBuilder> transforms = new LinkedHashMap<>();
         private final List<ElementBuilder> elements = new ArrayList<>();
         private ResourceLocation parent;
         private ResourceLocation renderType;
