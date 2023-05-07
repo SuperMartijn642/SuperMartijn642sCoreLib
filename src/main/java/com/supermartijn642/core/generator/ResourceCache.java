d467ea92669972144e21993221e26ae2da280e31
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-05-07 03:17:27 +0200

    Rework data generator processing and add ResourceAggregator

--- src/main/java/com/supermartijn642/core/generator/ResourceCache.java
+++ src/main/java/com/supermartijn642/core/generator/ResourceCache.java
@@ -5,21 +5,21 @@ import com.google.common.hash.Hashing;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
+import com.supermartijn642.core.generator.aggregator.ResourceAggregator;
+import com.supermartijn642.core.util.Pair;
 import net.minecraft.data.HashCache;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.resources.Resource;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created 16/08/2022 by SuperMartijn642
@@ -63,6 +63,17 @@ public abstract class ResourceCache {
     /**
      * Saves the given data in the appropriate location. Also checks if a file is already present to avoid redundant writes.
      * @param resourceType whether the given data is part of the server data or the client assets
+     * @param aggregator   aggregator used when multiple generator write to the same file location
+     * @param namespace    the namespace which the data should be saved under
+     * @param directory    name of the directory within the namespace
+     * @param fileName     name of the file
+     * @param extension    extension of the file
+     */
+    public abstract <T> void saveResource(ResourceType resourceType, ResourceAggregator<?,T> aggregator, T data, String namespace, String directory, String fileName, String extension);
+
+    /**
+     * Saves the given data in the appropriate location. Also checks if a file is already present to avoid redundant writes.
+     * @param resourceType whether the given data is part of the server data or the client assets
      * @param json         the data to be saved
      * @param namespace    the namespace which the data should be saved under
      * @param directory    name of the directory within the namespace
@@ -82,82 +93,183 @@ public abstract class ResourceCache {
      * @param extension    the file's extension
      * @return an input stream for the requested resource, or an empty optional if the resource does not exist
      */
-    public abstract Optional<InputStream> getManualResource(ResourceType resourceType, String namespace, String directory, String fileName, String extension);
+    public abstract Optional<InputStream> getExistingResource(ResourceType resourceType, String namespace, String directory, String fileName, String extension);
 
-    static ResourceCache wrap(ExistingFileHelper existingFileHelper, HashCache cachedOutput, Path outputDirectory){
-        return new ExistingFileHelperWrapper(existingFileHelper, cachedOutput, outputDirectory);
+    /**
+     * @deprecated for internal use only
+     */
+    @Deprecated
+    public static ResourceCache wrap(ExistingFileHelper existingFileHelper, HashCache hashCache, Path outputDirectory){
+        return new HashCacheWrapper(existingFileHelper, hashCache, outputDirectory);
     }
 
-    private static class ExistingFileHelperWrapper extends ResourceCache {
+    /**
+     * @deprecated for internal use only
+     */
+    @Deprecated
+    public static class HashCacheWrapper extends ResourceCache {
 
+        private final Map<Path,HashCode> presentFiles = new HashMap<>();
         private final Map<Path,HashCode> writtenFiles = new HashMap<>();
+        private final Map<Path,Pair<ResourceAggregator<Object,Object>,Object>> aggregatedResources = new HashMap<>();
+        private final List<Path> toBeGenerated = new ArrayList<>();
 
         private final ExistingFileHelper existingFileHelper;
         private final Path outputDirectory;
         private final HashCache cache;
+        private boolean allowWrites = true;
 
-        private ExistingFileHelperWrapper(ExistingFileHelper existingFileHelper, HashCache hashCache, Path outputFolder){
+        private HashCacheWrapper(ExistingFileHelper existingFileHelper, HashCache cache, Path outputFolder){
             if(outputFolder == null)
                 throw new IllegalArgumentException("Output directory must not be null!");
             this.outputDirectory = outputFolder;
             this.existingFileHelper = existingFileHelper;
-            this.cache = hashCache;
+            this.cache = cache;
+            // Copy all the paths from the hash cache
+            for(Map.Entry<Path,String> entry : this.cache.oldCache.entrySet())
+                this.presentFiles.put(this.outputDirectory.relativize(entry.getKey()), entry.getValue().isEmpty() ? HashCode.fromInt(0) : HashCode.fromString(entry.getValue()));
         }
 
-        @Override
-        public boolean doesResourceExist(ResourceType resourceType, String namespace, String directory, String fileName, String extension){
+        private boolean existsInGeneratedFiles(Path path){
+            return this.toBeGenerated.contains(path) || this.aggregatedResources.containsKey(path) || this.cache.newCache.containsKey(this.outputDirectory.resolve(path));
+        }
+
+        private boolean existsInLoadedResources(ResourceType resourceType, String namespace, String directory, String fileName, String extension){
             ResourceLocation location = new ResourceLocation(namespace, directory + "/" + fileName + extension);
             return this.existingFileHelper.exists(location, resourceType == ResourceType.DATA ? PackType.SERVER_DATA : PackType.CLIENT_RESOURCES);
-        }
-
-        @Override
-        public void trackToBeGeneratedResource(ResourceType resourceType, String namespace, String directory, String fileName, String extension){
-            ResourceLocation location = new ResourceLocation(namespace, fileName);
-            this.existingFileHelper.trackGenerated(location, resourceType == ResourceType.DATA ? PackType.SERVER_DATA : PackType.CLIENT_RESOURCES, extension, directory);
-        }
-
-        @Override
-        public Optional<InputStream> getManualResource(ResourceType resourceType, String namespace, String directory, String fileName, String extension){
-            Path path = this.constructPath(resourceType, namespace, directory, fileName, extension);
-            Path fullPath = this.outputDirectory.resolve(path);
-            if(!Files.exists(fullPath))
-                return Optional.empty();
-            try{
-                return Optional.of(Files.newInputStream(fullPath));
-            }catch(IOException e){
-                throw new RuntimeException(e);
-            }
         }
 
         private Path constructPath(ResourceType resourceType, String namespace, String directory, String fileName, String extension){
             return Paths.get(resourceType.getDirectoryName(), namespace, directory, fileName + extension);
         }
 
-        @SuppressWarnings("UnstableApiUsage")
+        @Override
+        public boolean doesResourceExist(ResourceType resourceType, String namespace, String directory, String fileName, String extension){
+            Path path = this.constructPath(resourceType, namespace, directory, fileName, extension);
+            return this.existsInGeneratedFiles(path)
+                || this.existsInLoadedResources(resourceType, namespace, directory, fileName, extension);
+        }
+
+        @Override
+        public void trackToBeGeneratedResource(ResourceType resourceType, String namespace, String directory, String fileName, String extension){
+            this.toBeGenerated.add(this.constructPath(resourceType, namespace, directory, fileName, extension));
+            ResourceLocation location = new ResourceLocation(namespace, directory + "/" + fileName + extension);
+            this.existingFileHelper.trackGenerated(location, resourceType == ResourceType.DATA ? PackType.SERVER_DATA : PackType.CLIENT_RESOURCES, extension, directory);
+        }
+
+        @Override
+        public Optional<InputStream> getExistingResource(ResourceType resourceType, String namespace, String directory, String fileName, String extension){
+            try{
+                Resource resource = this.existingFileHelper.getResource(new ResourceLocation(namespace, directory + "/" + fileName + extension), resourceType == ResourceType.DATA ? PackType.SERVER_DATA : PackType.CLIENT_RESOURCES);
+                return Optional.of(resource.getInputStream());
+            }catch(FileNotFoundException | NoSuchElementException e){
+                return Optional.empty();
+            }catch(IOException e){
+                throw new RuntimeException(e);
+            }
+        }
+
         @Override
         public void saveResource(ResourceType resourceType, byte[] data, String namespace, String directory, String fileName, String extension){
+            if(!this.allowWrites)
+                throw new RuntimeException("Resources cannot be saved during this stage!");
+
             Path path = this.constructPath(resourceType, namespace, directory, fileName, extension);
             Path fullPath = this.outputDirectory.resolve(path);
-            if(this.writtenFiles.containsKey(path) || this.cache.newCache.containsKey(fullPath))
+            if(this.writtenFiles.containsKey(path) || this.aggregatedResources.containsKey(path) || this.cache.newCache.containsKey(fullPath))
                 throw new RuntimeException("Duplicate file '" + path + "'!");
 
             // Skip writing if the present file matches the one to be written
             HashCode hashCode = Hashing.sha1().hashBytes(data);
-            if(hashCode.toString().equals(this.cache.getHash(fullPath)) && fullPath.toFile().exists()){
+            if(this.presentFiles.containsKey(path) && this.presentFiles.get(path).equals(hashCode) && fullPath.toFile().exists()){
                 this.writtenFiles.put(path, hashCode);
+                this.toBeGenerated.remove(path);
                 this.cache.putNew(fullPath, hashCode.toString());
                 return;
             }
 
             // Write the data to file
-            fullPath.getParent().toFile().mkdirs();
+            fullPath.toFile().getParentFile().mkdirs();
             try(OutputStream outputStream = Files.newOutputStream(fullPath)){
                 outputStream.write(data);
             }catch(IOException e){
                 throw new RuntimeException(e);
             }
             this.writtenFiles.put(path, hashCode);
+            this.toBeGenerated.remove(path);
             this.cache.putNew(fullPath, hashCode.toString());
+        }
+
+        @Override
+        public <T> void saveResource(ResourceType resourceType, ResourceAggregator<?,T> aggregator, T data, String namespace, String directory, String fileName, String extension){
+            if(!this.allowWrites)
+                throw new RuntimeException("Resources cannot be saved during this stage!");
+
+            Path path = this.constructPath(resourceType, namespace, directory, fileName, extension);
+            Path fullPath = this.outputDirectory.resolve(path);
+            if(this.writtenFiles.containsKey(path) || this.cache.newCache.containsKey(fullPath))
+                throw new RuntimeException("Duplicate file '" + path + "'!");
+
+            // Validate the aggregators match
+            Pair<ResourceAggregator<Object,Object>,Object> oldEntry = this.aggregatedResources.get(path);
+            if(oldEntry != null && oldEntry.left() != aggregator)
+                throw new RuntimeException("Incompatible aggregators for file '" + path + "': '" + oldEntry.left().getClass() + "' and '" + aggregator.getClass() + "'!");
+
+            // Combine the old with the new data
+            Object oldData = oldEntry == null ? aggregator.initialData() : oldEntry.right();
+            try{
+                //noinspection unchecked
+                oldData = ((ResourceAggregator<Object,Object>)aggregator).combine(oldData, data);
+            }catch(Exception e){
+                throw new RuntimeException("Failed to combine data for file '" + path + "'!", e);
+            }
+            //noinspection unchecked
+            this.aggregatedResources.put(path, Pair.of((ResourceAggregator<Object,Object>)aggregator, oldData));
+        }
+
+        public void allowWrites(boolean allow){
+            this.allowWrites = allow;
+        }
+
+        public void finish(){
+            // Write all aggregated resources
+            this.aggregatedResources.forEach((path, pair) -> {
+                // Convert the data to bytes
+                ResourceAggregator<Object,Object> aggregator = pair.left();
+                Object data = pair.right();
+                byte[] bytes;
+                try(ByteArrayOutputStream stream = new ByteArrayOutputStream()){
+                    aggregator.write(stream, data);
+                    bytes = stream.toByteArray();
+                }catch(Exception e){
+                    throw new RuntimeException(e);
+                }
+
+                // Skip writing if the present file matches the one to be written
+                Path fullPath = this.outputDirectory.resolve(path);
+                HashCode hashCode = Hashing.sha1().hashBytes(bytes);
+                if(this.presentFiles.containsKey(path) && this.presentFiles.get(path).equals(hashCode) && fullPath.toFile().exists()){
+                    this.writtenFiles.put(path, hashCode);
+                    this.toBeGenerated.remove(path);
+                    this.cache.putNew(fullPath, hashCode.toString());
+                    return;
+                }
+
+                // Write the data to file
+                fullPath.toFile().getParentFile().mkdirs();
+                try(OutputStream outputStream = Files.newOutputStream(fullPath)){
+                    outputStream.write(bytes);
+                }catch(IOException e){
+                    throw new RuntimeException(e);
+                }
+                this.writtenFiles.put(path, hashCode);
+                this.toBeGenerated.remove(path);
+                this.cache.putNew(fullPath, hashCode.toString());
+            });
+
+            // Validate all promised files have actually been written
+            if(!this.toBeGenerated.isEmpty())
+                throw new RuntimeException("Some tracked files did not get written: " + this.toBeGenerated.stream().map(Path::toString).map(s -> "'" + s + "'").collect(Collectors.joining(",")));
         }
     }
 }
--- src/main/java/com/supermartijn642/core/generator/TagGenerator.java
+++ src/main/java/com/supermartijn642/core/generator/TagGenerator.java
@@ -1,7 +1,10 @@
 package com.supermartijn642.core.generator;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.supermartijn642.core.generator.aggregator.ResourceAggregator;
 import com.supermartijn642.core.registry.Registries;
 import com.supermartijn642.core.registry.RegistryUtil;
 import net.minecraft.resources.ResourceLocation;
@@ -12,6 +15,10 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -21,6 +28,66 @@ import java.util.Set;
  * Created 05/08/2022 by SuperMartijn642
  */
 public abstract class TagGenerator extends ResourceGenerator {
+
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
+    private static final ResourceAggregator<TagBuilder<?>,TagBuilder<?>> AGGREGATOR = new ResourceAggregator<>() {
+        @Override
+        public TagBuilder<?> initialData(){
+            return null;
+        }
+
+        @Override
+        public TagBuilder<?> combine(TagBuilder<?> data, TagBuilder<?> newData){
+            if(data != null){
+                //noinspection unchecked,rawtypes
+                ((TagBuilder)data).addAll(newData);
+                return data;
+            }
+            return newData;
+        }
+
+        @Override
+        public void write(OutputStream stream, TagBuilder<?> tag) throws IOException{
+            // Convert the tag into a json object
+            JsonObject json = new JsonObject();
+            // Replace
+            json.addProperty("replace", tag.replace);
+            // Entries & references
+            JsonArray entries = new JsonArray();
+            tag.entries.forEach(entries::add);
+            tag.references.stream().map(ResourceLocation::toString).map(s -> "#" + s).forEach(entries::add);
+            tag.optionalEntries.stream().map(e -> {
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("id", e);
+                jsonObject.addProperty("required", false);
+                return jsonObject;
+            }).forEach(entries::add);
+            tag.optionalReferences.stream().map(e -> {
+                JsonObject entryObject = new JsonObject();
+                entryObject.addProperty("id", "#" + e);
+                entryObject.addProperty("required", false);
+                return entryObject;
+            }).forEach(entries::add);
+            if(entries.size() > 0 || tag.remove.isEmpty())
+                json.add("values", entries);
+            // Removed
+            JsonArray removedEntries = new JsonArray();
+            tag.remove.forEach(removedEntries::add);
+            tag.optionalRemove.stream().map(e -> {
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("id", e);
+                jsonObject.addProperty("required", false);
+                return jsonObject;
+            }).forEach(entries::add);
+            if(removedEntries.size() > 0)
+                json.add("remove", removedEntries);
+
+            // Write the data
+            try(Writer writer = new OutputStreamWriter(stream)){
+                GSON.toJson(json, writer);
+            }
+        }
+    };
 
     private final Map<Registries.Registry<?>,Map<ResourceLocation,TagBuilder<?>>> tags = new HashMap<>();
 
@@ -41,49 +108,14 @@ public abstract class TagGenerator extends ResourceGenerator {
                 for(ResourceLocation reference : tag.references){
                     if(registryEntry.getValue().containsKey(reference))
                         continue;
-                    if(this.cache.doesResourceExist(ResourceType.DATA, reference.getNamespace(), directoryName, reference.getPath(), ".json"))
+                    if(this.cache.doesResourceExist(ResourceType.DATA, reference.getNamespace(), "tags/" + directoryName, reference.getPath(), ".json"))
                         continue;
 
                     throw new RuntimeException("Could not find tag reference '" + reference + "'!");
                 }
-
-                // Convert the tag into a json object
-                JsonObject object = new JsonObject();
-                // Replace
-                object.addProperty("replace", tag.replace);
-                // Entries & references
-                JsonArray entries = new JsonArray();
-                tag.entries.forEach(entries::add);
-                tag.references.stream().map(ResourceLocation::toString).map(s -> "#" + s).forEach(entries::add);
-                tag.optionalEntries.stream().map(e -> {
-                    JsonObject jsonObject = new JsonObject();
-                    jsonObject.addProperty("id", e);
-                    jsonObject.addProperty("required", false);
-                    return jsonObject;
-                }).forEach(entries::add);
-                tag.optionalReferences.stream().map(e -> {
-                    JsonObject entryObject = new JsonObject();
-                    entryObject.addProperty("id", "#" + e);
-                    entryObject.addProperty("required", false);
-                    return entryObject;
-                }).forEach(entries::add);
-                if(entries.size() > 0 || tag.remove.isEmpty())
-                    object.add("values", entries);
-                // Removed
-                JsonArray removedEntries = new JsonArray();
-                tag.remove.forEach(removedEntries::add);
-                tag.optionalRemove.stream().map(e -> {
-                    JsonObject jsonObject = new JsonObject();
-                    jsonObject.addProperty("id", e);
-                    jsonObject.addProperty("required", false);
-                    return jsonObject;
-                }).forEach(entries::add);
-                if(removedEntries.size() > 0)
-                    object.add("remove", removedEntries);
-
                 // Save the object to the cache
                 ResourceLocation identifier = tag.identifier;
-                this.cache.saveJsonResource(ResourceType.DATA, object, identifier.getNamespace(), "tags/" + directoryName, identifier.getPath());
+                this.cache.saveResource(ResourceType.DATA, AGGREGATOR, tag, identifier.getNamespace(), "tags/" + directoryName, identifier.getPath(), ".json");
             }
         }
     }
@@ -580,6 +612,15 @@ public abstract class TagGenerator extends ResourceGenerator {
 
             this.removeOptional(new ResourceLocation(entry));
             return this;
+        }
+
+        private void addAll(TagBuilder<T> other){
+            this.entries.addAll(other.entries);
+            this.optionalEntries.addAll(other.optionalEntries);
+            this.references.addAll(other.references);
+            this.optionalReferences.addAll(other.optionalReferences);
+            this.remove.addAll(other.remove);
+            this.optionalRemove.addAll(other.optionalRemove);
         }
     }
 }
