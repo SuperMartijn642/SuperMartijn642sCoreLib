e7f49d2b42c27659fcb16c52ad4b88f441e2fe5d
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-05-15 20:41:55 +0200

    Change HashCacheWrapper#toBeGenerated to be a set

--- src/main/java/com/supermartijn642/core/generator/ResourceCache.java
+++ src/main/java/com/supermartijn642/core/generator/ResourceCache.java
@@ -107,7 +107,7 @@ public abstract class ResourceCache {
         private final Map<Path,HashCode> presentFiles = new HashMap<>();
         private final Map<Path,HashCode> writtenFiles = new HashMap<>();
         private final Map<Path,Pair<ResourceAggregator<Object,Object>,Object>> aggregatedResources = new HashMap<>();
-        private final List<Path> toBeGenerated = new ArrayList<>();
+        private final Set<Path> toBeGenerated = new HashSet<>();
 
         private final ExistingFileHelper existingFileHelper;
         private final Path outputDirectory;
