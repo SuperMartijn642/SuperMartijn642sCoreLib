d13d44be38f736befe49ffa2351f739fc80d9ebf
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-05-19 18:43:37 +0200

    Fix MappedRegistryMixin

--- src/main/java/com/supermartijn642/core/mixin/MappedRegistryMixin.java
+++ src/main/java/com/supermartijn642/core/mixin/MappedRegistryMixin.java
@@ -3,11 +3,9 @@ package com.supermartijn642.core.mixin;
 import com.mojang.serialization.Lifecycle;
 import com.supermartijn642.core.extensions.CoreLibHolderReference;
 import com.supermartijn642.core.extensions.CoreLibMappedRegistry;
-import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import net.minecraft.core.Holder;
 import net.minecraft.core.MappedRegistry;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -15,8 +13,8 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 /**
  * Created 22/03/2023 by SuperMartijn642
@@ -25,49 +23,35 @@ import java.util.Map;
 public class MappedRegistryMixin implements CoreLibMappedRegistry {
 
     @Shadow
-    private Object2IntMap<?> toId;
+    private Function<?,Holder.Reference<?>> customHolderProvider;
     @Shadow
-    private Map<ResourceLocation,Holder.Reference<?>> byLocation;
-    @Shadow
-    private Map<?, Holder.Reference<?>> byValue;
-    @Shadow
-    private Map<?, Lifecycle> lifecycles;
-    @Unique
-    private final Map<ResourceKey<?>,Object> keyToObject = new HashMap<>();
+    private Map<ResourceKey<?>,Holder.Reference<?>> byKey;
     @Unique
     private boolean registeringOverrides = false;
     @Unique
     private Holder.Reference<?> overwrittenReference;
 
     @Inject(
-        method = "registerMapping(ILnet/minecraft/resources/ResourceKey;Ljava/lang/Object;Lcom/mojang/serialization/Lifecycle;)Lnet/minecraft/core/Holder$Reference;",
+        method = "registerMapping(ILnet/minecraft/resources/ResourceKey;Ljava/lang/Object;Lcom/mojang/serialization/Lifecycle;Z)Lnet/minecraft/core/Holder;",
         at = @At("HEAD")
     )
-    private void registerMappingHead(int id, ResourceKey<?> key, Object object, Lifecycle lifecycle, CallbackInfoReturnable<Holder<?>> ci){
-        if(this.registeringOverrides)
-            this.overwrittenReference = this.byLocation.remove(key.location());
+    private void registerMappingHead(int id, ResourceKey<?> key, Object object, Lifecycle lifecycle, boolean checkDuplicates, CallbackInfoReturnable<Holder<?>> ci){
+        if(this.registeringOverrides && this.customHolderProvider != null)
+            this.overwrittenReference = this.byKey.remove(key);
     }
 
     @Inject(
-        method = "registerMapping(ILnet/minecraft/resources/ResourceKey;Ljava/lang/Object;Lcom/mojang/serialization/Lifecycle;)Lnet/minecraft/core/Holder$Reference;",
+        method = "registerMapping(ILnet/minecraft/resources/ResourceKey;Ljava/lang/Object;Lcom/mojang/serialization/Lifecycle;Z)Lnet/minecraft/core/Holder;",
         at = @At("TAIL")
     )
-    private void registerMappingTail(int id, ResourceKey<?> key, Object object, Lifecycle lifecycle, CallbackInfoReturnable<Holder<?>> ci){
-        if(this.registeringOverrides){
-            // Remove the old object
-            Object oldValue = this.keyToObject.get(key);
-            if(oldValue != null){
-                this.toId.removeInt(oldValue);
-                this.byValue.remove(oldValue);
-                this.lifecycles.remove(oldValue);
-            }
-            // Redirect the old reference to the new reference's values
-            Holder.Reference<?> newReference = this.byLocation.get(key.location());
-            if(newReference != null && newReference != this.overwrittenReference)
+    private void registerMappingTail(int id, ResourceKey<?> key, Object object, Lifecycle lifecycle, boolean checkDuplicates, CallbackInfoReturnable<Holder<?>> ci){
+        if(this.registeringOverrides && this.customHolderProvider != null && this.overwrittenReference != null){
+            Holder.Reference<?> newReference = this.byKey.get(key);
+            if(newReference != null && newReference != this.overwrittenReference){
+                // Redirect the old reference to the new reference's values
                 ((CoreLibHolderReference)this.overwrittenReference).supermartijn642corelibOverride(key, object);
+            }
         }
-
-        this.keyToObject.put(key, object);
     }
 
     @Override
