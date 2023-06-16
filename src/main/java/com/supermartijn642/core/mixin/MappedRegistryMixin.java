47770e1d2f74aab063b0d087c6a5aa7551c9cecb
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-06-16 16:59:28 +0200

    Make registry overrides replace values in vanilla fields

--- src/main/java/com/supermartijn642/core/mixin/MappedRegistryMixin.java
+++ src/main/java/com/supermartijn642/core/mixin/MappedRegistryMixin.java
@@ -17,6 +17,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.BiConsumer;
 
 /**
  * Created 22/03/2023 by SuperMartijn642
@@ -29,15 +30,16 @@ public class MappedRegistryMixin implements CoreLibMappedRegistry {
     @Shadow
     private Map<ResourceLocation,Holder.Reference<?>> byLocation;
     @Shadow
-    private Map<?, Holder.Reference<?>> byValue;
+    private Map<Object,Holder.Reference<?>> byValue;
     @Shadow
-    private Map<?, Lifecycle> lifecycles;
+    private Map<?,Lifecycle> lifecycles;
     @Unique
     private final Map<ResourceKey<?>,Object> keyToObject = new HashMap<>();
     @Unique
     private boolean registeringOverrides = false;
     @Unique
     private Holder.Reference<?> overwrittenReference;
+    private BiConsumer<Object,Object> overrideConsumer;
 
     @Inject(
         method = "registerMapping(ILnet/minecraft/resources/ResourceKey;Ljava/lang/Object;Lcom/mojang/serialization/Lifecycle;)Lnet/minecraft/core/Holder$Reference;",
@@ -54,23 +56,34 @@ public class MappedRegistryMixin implements CoreLibMappedRegistry {
     )
     private void registerMappingTail(int id, ResourceKey<?> key, Object object, Lifecycle lifecycle, CallbackInfoReturnable<Holder<?>> ci){
         if(this.registeringOverrides){
-            // Remove the old object
-            Object oldValue = this.keyToObject.get(key);
-            if(oldValue != null){
-                this.toId.removeInt(oldValue);
-                this.lifecycles.remove(oldValue);
-            }
             // Redirect the old reference to the new reference's values
             Holder.Reference<?> newReference = this.byLocation.get(key.location());
             if(newReference != null && newReference != this.overwrittenReference)
                 ((CoreLibHolderReference)this.overwrittenReference).supermartijn642corelibOverride(key, object);
+            // Remove the old object
+            Object oldValue = this.keyToObject.get(key);
+            if(oldValue != null){
+                this.toId.removeInt(oldValue);
+                // Create a dummy reference
+                //noinspection unchecked, DataFlowIssue, rawtypes, deprecation
+                Holder.Reference dummy = Holder.Reference.createIntrusive(((MappedRegistry)(Object)this).holderOwner(), oldValue);
+                //noinspection unchecked
+                dummy.bindKey(this.overwrittenReference.key());
+                this.byValue.put(oldValue, dummy);
+                this.lifecycles.remove(oldValue);
+
+                // Call the override consumer
+                if(this.overrideConsumer != null)
+                    this.overrideConsumer.accept(oldValue, object);
+            }
         }
 
         this.keyToObject.put(key, object);
     }
 
     @Override
-    public void supermartijn642corelibSetRegisterOverrides(boolean flag){
+    public void supermartijn642corelibSetRegisterOverrides(boolean flag, BiConsumer<Object,Object> overrideConsumer){
         this.registeringOverrides = flag;
+        this.overrideConsumer = overrideConsumer;
     }
 }
