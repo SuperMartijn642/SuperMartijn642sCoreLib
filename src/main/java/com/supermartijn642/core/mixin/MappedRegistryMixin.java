9ea97521d9a345c0927ba762041bc8fb8e663643
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-06-16 16:59:28 +0200

    Make registry overrides replace values in vanilla fields

--- src/main/java/com/supermartijn642/core/mixin/MappedRegistryMixin.java
+++ src/main/java/com/supermartijn642/core/mixin/MappedRegistryMixin.java
@@ -14,6 +14,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Map;
+import java.util.function.BiConsumer;
 import java.util.function.Function;
 
 /**
@@ -32,6 +33,7 @@ public class MappedRegistryMixin implements CoreLibMappedRegistry {
     private boolean registeringOverrides = false;
     @Unique
     private Holder.Reference<?> overwrittenReference;
+    private BiConsumer<Object,Object> overrideConsumer;
 
     @Inject(
         method = "registerMapping(ILnet/minecraft/resources/ResourceKey;Ljava/lang/Object;Lcom/mojang/serialization/Lifecycle;Z)Lnet/minecraft/core/Holder;",
@@ -47,7 +49,7 @@ public class MappedRegistryMixin implements CoreLibMappedRegistry {
         at = @At("TAIL")
     )
     private void registerMappingTail(int id, ResourceKey<?> key, Object object, Lifecycle lifecycle, boolean checkDuplicates, CallbackInfoReturnable<Holder<?>> ci){
-        if(this.registeringOverrides && this.customHolderProvider != null && this.overwrittenReference != null){
+        if(this.registeringOverrides && this.overwrittenReference != null){
             Holder.Reference<?> newReference = this.byKey.get(key);
             if(newReference != null && newReference != this.overwrittenReference){
                 // Add back the old entry to the 'byValue' map
@@ -55,11 +57,15 @@ public class MappedRegistryMixin implements CoreLibMappedRegistry {
                 // Redirect the old reference to the new reference's values
                 ((CoreLibHolderReference)this.overwrittenReference).supermartijn642corelibOverride(key, object);
             }
+            // Call the override consumer
+            if(this.overrideConsumer != null)
+                this.overrideConsumer.accept(this.overwrittenReference.value(), object);
         }
     }
 
     @Override
-    public void supermartijn642corelibSetRegisterOverrides(boolean flag){
+    public void supermartijn642corelibSetRegisterOverrides(boolean flag, BiConsumer<Object,Object> overrideConsumer){
         this.registeringOverrides = flag;
+        this.overrideConsumer = overrideConsumer;
     }
 }
--- src/main/java/com/supermartijn642/core/registry/Registries.java
+++ src/main/java/com/supermartijn642/core/registry/Registries.java
@@ -31,6 +31,7 @@ import net.minecraft.world.level.material.Fluid;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
+import java.util.function.BiConsumer;
 
 import static net.minecraft.core.Registry.*;
 
@@ -69,22 +70,22 @@ public final class Registries {
         return IDENTIFIER_TO_REGISTRY.get(identifier);
     }
 
-    public static final Registry<Block> BLOCKS = vanilla(BLOCK, Block.class);
-    public static final Registry<Fluid> FLUIDS = vanilla(FLUID, Fluid.class);
-    public static final Registry<Item> ITEMS = vanilla(ITEM, Item.class);
-    public static final Registry<MobEffect> MOB_EFFECTS = vanilla(MOB_EFFECT, MobEffect.class);
-    public static final Registry<SoundEvent> SOUND_EVENTS = vanilla(SOUND_EVENT, SoundEvent.class);
-    public static final Registry<Potion> POTIONS = vanilla(POTION, Potion.class);
-    public static final Registry<Enchantment> ENCHANTMENTS = vanilla(ENCHANTMENT, Enchantment.class);
-    public static final Registry<EntityType<?>> ENTITY_TYPES = vanilla(ENTITY_TYPE, EntityType.class);
-    public static final Registry<BlockEntityType<?>> BLOCK_ENTITY_TYPES = vanilla(BLOCK_ENTITY_TYPE, BlockEntityType.class);
-    public static final Registry<ParticleType<?>> PARTICLE_TYPES = vanilla(PARTICLE_TYPE, ParticleType.class);
-    public static final Registry<MenuType<?>> MENU_TYPES = vanilla(MENU, MenuType.class);
+    public static final Registry<Block> BLOCKS = vanilla(BLOCK, Block.class, RegistryOverrideHandlers.BLOCKS);
+    public static final Registry<Fluid> FLUIDS = vanilla(FLUID, Fluid.class, RegistryOverrideHandlers.FLUIDS);
+    public static final Registry<Item> ITEMS = vanilla(ITEM, Item.class, RegistryOverrideHandlers.ITEMS);
+    public static final Registry<MobEffect> MOB_EFFECTS = vanilla(MOB_EFFECT, MobEffect.class, RegistryOverrideHandlers.MOB_EFFECTS);
+    public static final Registry<SoundEvent> SOUND_EVENTS = vanilla(SOUND_EVENT, SoundEvent.class, RegistryOverrideHandlers.SOUND_EVENTS);
+    public static final Registry<Potion> POTIONS = vanilla(POTION, Potion.class, RegistryOverrideHandlers.POTIONS);
+    public static final Registry<Enchantment> ENCHANTMENTS = vanilla(ENCHANTMENT, Enchantment.class, RegistryOverrideHandlers.ENCHANTMENTS);
+    public static final Registry<EntityType<?>> ENTITY_TYPES = vanilla(ENTITY_TYPE, EntityType.class, RegistryOverrideHandlers.ENTITY_TYPES);
+    public static final Registry<BlockEntityType<?>> BLOCK_ENTITY_TYPES = vanilla(BLOCK_ENTITY_TYPE, BlockEntityType.class, RegistryOverrideHandlers.BLOCK_ENTITY_TYPES);
+    public static final Registry<ParticleType<?>> PARTICLE_TYPES = vanilla(PARTICLE_TYPE, ParticleType.class, RegistryOverrideHandlers.PARTICLE_TYPES);
+    public static final Registry<MenuType<?>> MENU_TYPES = vanilla(MENU, MenuType.class, RegistryOverrideHandlers.MENU_TYPES);
     public static final Registry<Motive> PAINTING_VARIANTS = vanilla(MOTIVE, Motive.class);
-    public static final Registry<RecipeType<?>> RECIPE_TYPES = vanilla(RECIPE_TYPE, RecipeType.class);
-    public static final Registry<RecipeSerializer<?>> RECIPE_SERIALIZERS = vanilla(RECIPE_SERIALIZER, RecipeSerializer.class);
-    public static final Registry<Attribute> ATTRIBUTES = vanilla(ATTRIBUTE, Attribute.class);
-    public static final Registry<StatType<?>> STAT_TYPES = vanilla(STAT_TYPE, StatType.class);
+    public static final Registry<RecipeType<?>> RECIPE_TYPES = vanilla(RECIPE_TYPE, RecipeType.class, RegistryOverrideHandlers.RECIPE_TYPES);
+    public static final Registry<RecipeSerializer<?>> RECIPE_SERIALIZERS = vanilla(RECIPE_SERIALIZER, RecipeSerializer.class, RegistryOverrideHandlers.RECIPE_SERIALIZERS);
+    public static final Registry<Attribute> ATTRIBUTES = vanilla(ATTRIBUTE, Attribute.class, RegistryOverrideHandlers.ATTRIBUTES);
+    public static final Registry<StatType<?>> STAT_TYPES = vanilla(STAT_TYPE, StatType.class, RegistryOverrideHandlers.STAT_TYPES);
     public static final Registry<ResourceConditionSerializer<?>> RESOURCE_CONDITION_SERIALIZERS = new MapBackedRegistry<>(new ResourceLocation("supermartijn642corelib", "resource_conditions"), ResourceConditionSerializer.class) {
         @Override
         public void register(ResourceLocation identifier, ResourceConditionSerializer<?> object){
@@ -122,15 +123,20 @@ public final class Registries {
         );
     }
 
+    private static <T> Registry<T> vanilla(net.minecraft.core.Registry<T> registry, Class<? super T> valueClass, BiConsumer<Object,Object> overrideConsumer){
+        return new VanillaRegistryWrapper<>(registry, valueClass, overrideConsumer);
+    }
+
     private static <T> Registry<T> vanilla(net.minecraft.core.Registry<T> registry, Class<? super T> valueClass){
-        return new VanillaRegistryWrapper<>(registry, valueClass);
+        return vanilla(registry, valueClass, null);
     }
 
     public interface Registry<T> {
 
         ResourceLocation getRegistryIdentifier();
 
-        @Nullable net.minecraft.core.Registry<T> getVanillaRegistry();
+        @Nullable
+        net.minecraft.core.Registry<T> getVanillaRegistry();
 
         boolean hasVanillaRegistry();
 
@@ -156,12 +162,14 @@ public final class Registries {
         private final net.minecraft.core.Registry<T> registry;
         private final ResourceLocation identifier;
         private final Class<T> valueClass;
+        private final BiConsumer<Object,Object> overrideConsumer;
 
-        private VanillaRegistryWrapper(net.minecraft.core.Registry<T> registry, Class<? super T> valueClass){
+        private VanillaRegistryWrapper(net.minecraft.core.Registry<T> registry, Class<? super T> valueClass, BiConsumer<Object,Object> overrideConsumer){
             this.registry = registry;
             this.identifier = registry.key().location();
             //noinspection unchecked
             this.valueClass = (Class<T>)valueClass;
+            this.overrideConsumer = overrideConsumer;
 
             addRegistry(this);
         }
@@ -187,9 +195,9 @@ public final class Registries {
                 ResourceKey<T> key = ResourceKey.create(this.registry.key(), identifier);
                 T oldObject = this.registry.get(key);
                 int id = this.registry.getId(oldObject);
-                ((CoreLibMappedRegistry)this.registry).supermartijn642corelibSetRegisterOverrides(true);
+                ((CoreLibMappedRegistry)this.registry).supermartijn642corelibSetRegisterOverrides(true, this.overrideConsumer);
                 ((MappedRegistry<T>)this.registry).registerOrOverride(OptionalInt.of(id), key, object, Lifecycle.stable());
-                ((CoreLibMappedRegistry)this.registry).supermartijn642corelibSetRegisterOverrides(false);
+                ((CoreLibMappedRegistry)this.registry).supermartijn642corelibSetRegisterOverrides(false, null);
                 return;
             }
             net.minecraft.core.Registry.register(this.registry, identifier, object);
