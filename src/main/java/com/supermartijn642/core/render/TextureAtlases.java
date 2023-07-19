e6f2dde3e99434d9ca7fe3cfe54b92df7b208a5e
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-07-20 00:16:41 +0200

    Hardcode texture atlas locations in `TextureAtlases` to prevent loading the `Sheets` class #174

--- src/main/java/com/supermartijn642/core/render/TextureAtlases.java
+++ src/main/java/com/supermartijn642/core/render/TextureAtlases.java
@@ -1,6 +1,5 @@
 package com.supermartijn642.core.render;
 
-import net.minecraft.client.renderer.texture.AtlasTexture;
 import net.minecraft.util.ResourceLocation;
 
 /**
@@ -8,8 +7,8 @@ import net.minecraft.util.ResourceLocation;
  */
 public class TextureAtlases {
 
-    private static final ResourceLocation BLOCKS = AtlasTexture.LOCATION_BLOCKS;
-    private static final ResourceLocation PARTICLES = AtlasTexture.LOCATION_PARTICLES;
+    private static final ResourceLocation BLOCKS = new ResourceLocation("textures/atlas/blocks.png");
+    private static final ResourceLocation PARTICLES = new ResourceLocation("textures/atlas/particles.png");
     private static final ResourceLocation MOB_EFFECTS = new ResourceLocation("textures/atlas/mob_effects.png");
     private static final ResourceLocation PAINTINGS = new ResourceLocation("textures/atlas/paintings.png");
 
