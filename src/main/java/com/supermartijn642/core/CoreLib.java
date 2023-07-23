e98f16ab05809f1cd17f81dbcba648c6438098a7
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-07-23 16:05:59 +0200

    Add CommonUtils#getLogger

--- src/main/java/com/supermartijn642/core/CoreLib.java
+++ src/main/java/com/supermartijn642/core/CoreLib.java
@@ -14,7 +14,6 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLConstructModEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.function.Consumer;
 
@@ -24,7 +23,7 @@ import java.util.function.Consumer;
 @Mod("supermartijn642corelib")
 public class CoreLib {
 
-    public static final Logger LOGGER = LoggerFactory.getLogger("supermartijn642corelib");
+    public static final Logger LOGGER = CommonUtils.getLogger("supermartijn642corelib");
 
     public CoreLib(){
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onConstructMod);
