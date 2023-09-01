9c31d2676f968708082400ae46b627787eb883f9
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-09-01 18:27:17 +0200

    Move pre launch entrypoint to its own class

--- src/main/java/com/supermartijn642/core/CoreLib.java
+++ src/main/java/com/supermartijn642/core/CoreLib.java
@@ -12,26 +12,12 @@ import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.loader.api.FabricLoader;
-import net.fabricmc.loader.api.LanguageAdapter;
-import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
-import net.fabricmc.loader.impl.FabricLoaderImpl;
-import net.fabricmc.loader.impl.ModContainerImpl;
-import net.fabricmc.loader.impl.entrypoint.EntrypointStorage;
-import net.fabricmc.loader.impl.util.DefaultLanguageAdapter;
-import org.quiltmc.loader.api.QuiltLoader;
-import org.quiltmc.loader.api.plugin.ModContainerExt;
-import org.quiltmc.loader.impl.QuiltLoaderImpl;
 import org.slf4j.Logger;
-
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Created 18/03/2022 by SuperMartijn642
  */
-public class CoreLib implements ModInitializer, PreLaunchEntrypoint {
+public class CoreLib implements ModInitializer {
 
     public static final Logger LOGGER = CommonUtils.getLogger("supermartijn642corelib");
 
@@ -73,73 +59,6 @@ public class CoreLib implements ModInitializer, PreLaunchEntrypoint {
             }catch(Exception ignore){}
             if(testMod != null)
                 testMod.onInitialize();
-        }
-    }
-
-    @Override
-    public void onPreLaunch(){
-        try{
-            // Check if Quilt loader is present
-            boolean quiltLoaded = false;
-            try{
-                Class.forName("org.quiltmc.loader.impl.QuiltLoaderImpl");
-                quiltLoaded = true;
-            }catch(ClassNotFoundException ignore){}
-
-            if(quiltLoaded){ // Quilt loader
-                Field storageField = QuiltLoaderImpl.class.getDeclaredField("entrypointStorage");
-                storageField.setAccessible(true);
-                Class<?> entrypointStorageClass = Class.forName("org.quiltmc.loader.impl.entrypoint.EntrypointStorage");
-                Field entriesField = entrypointStorageClass.getDeclaredField("entryMap");
-                entriesField.setAccessible(true);
-                Class<?> entryClass = Class.forName("org.quiltmc.loader.impl.entrypoint.EntrypointStorage$NewEntry");
-                Constructor<?> entryConstructor = entryClass.getDeclaredConstructors()[0];
-                entryConstructor.setAccessible(true);
-
-                // Get the entrypoint map
-                Object storage = storageField.get(QuiltLoaderImpl.INSTANCE);
-                //noinspection unchecked
-                Map<String,List<Object>> entries = (Map<String,List<Object>>)entriesField.get(storage);
-
-                // Get the core lib mod container
-                //noinspection OptionalGetWithoutIsPresent
-                ModContainerExt coreLibContainer = QuiltLoader.getModContainer("supermartijn642corelib").map(ModContainerExt.class::cast).get();
-                org.quiltmc.loader.api.LanguageAdapter languageAdapter = org.quiltmc.loader.impl.util.DefaultLanguageAdapter.INSTANCE;
-
-                // Add 'main' entrypoint
-                Object mainEntrypoint = entryConstructor.newInstance(coreLibContainer, languageAdapter, RegistryEntryPoints.class.getCanonicalName());
-                entries.get("main").add(mainEntrypoint);
-                // Add 'client' entrypoint
-                Object clientEntrypoint = entryConstructor.newInstance(coreLibContainer, languageAdapter, RegistryEntryPoints.class.getCanonicalName());
-                entries.get("client").add(clientEntrypoint);
-            }else{ // Fabric loader
-                Field storageField = FabricLoaderImpl.class.getDeclaredField("entrypointStorage");
-                storageField.setAccessible(true);
-                Field entriesField = EntrypointStorage.class.getDeclaredField("entryMap");
-                entriesField.setAccessible(true);
-                Class<?> entryClass = Class.forName("net.fabricmc.loader.impl.entrypoint.EntrypointStorage$NewEntry");
-                Constructor<?> entryConstructor = entryClass.getDeclaredConstructor(ModContainerImpl.class, LanguageAdapter.class, String.class);
-                entryConstructor.setAccessible(true);
-
-                // Get the entrypoint map
-                EntrypointStorage storage = (EntrypointStorage)storageField.get(FabricLoaderImpl.INSTANCE);
-                //noinspection unchecked
-                Map<String,List<Object>> entries = (Map<String,List<Object>>)entriesField.get(storage);
-
-                // Get the core lib mod container
-                //noinspection OptionalGetWithoutIsPresent
-                ModContainerImpl coreLibContainer = FabricLoader.getInstance().getModContainer("supermartijn642corelib").map(ModContainerImpl.class::cast).get();
-                LanguageAdapter languageAdapter = DefaultLanguageAdapter.INSTANCE;
-
-                // Add 'main' entrypoint
-                Object mainEntrypoint = entryConstructor.newInstance(coreLibContainer, languageAdapter, RegistryEntryPoints.class.getCanonicalName());
-                entries.get("main").add(mainEntrypoint);
-                // Add 'client' entrypoint
-                Object clientEntrypoint = entryConstructor.newInstance(coreLibContainer, languageAdapter, RegistryEntryPoints.class.getCanonicalName());
-                entries.get("client").add(clientEntrypoint);
-            }
-        }catch(Exception e){
-            throw new RuntimeException("Failed to apply Core Lib registry entry points!", e);
         }
     }
 
