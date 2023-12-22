cec7abd2750c86b88f167056fc05fe44180d3694
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-12-22 18:24:56 +0100

    Target NeoForge's overwrite for the `RecipeManagerMixin`

--- src/main/java/com/supermartijn642/core/mixin/RecipeManagerMixin.java
+++ src/main/java/com/supermartijn642/core/mixin/RecipeManagerMixin.java
@@ -1,6 +1,7 @@
 package com.supermartijn642.core.mixin;
 
 import com.google.gson.JsonObject;
+import com.mojang.serialization.DynamicOps;
 import com.supermartijn642.core.data.recipe.ConditionalRecipeSerializer;
 import com.supermartijn642.core.registry.Registries;
 import com.supermartijn642.core.registry.RegistryUtil;
@@ -12,6 +13,8 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Optional;
+
 /**
  * Created 12/09/2023 by SuperMartijn642
  */
@@ -19,16 +22,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class RecipeManagerMixin {
 
     @Inject(
-        method = "fromJson(Lnet/minecraft/resources/ResourceLocation;Lcom/google/gson/JsonObject;)Lnet/minecraft/world/item/crafting/RecipeHolder;",
+        method = "fromJson(Lnet/minecraft/resources/ResourceLocation;Lcom/google/gson/JsonObject;Lcom/mojang/serialization/DynamicOps;)Ljava/util/Optional;",
         at = @At("HEAD"),
         cancellable = true
     )
-    private static void fromJson(ResourceLocation recipeLocation, JsonObject json, CallbackInfoReturnable<RecipeHolder<?>> ci){
+    private static void fromJson(ResourceLocation recipeLocation, JsonObject json, DynamicOps<?> ops, CallbackInfoReturnable<Optional<RecipeHolder<?>>> ci){
         // Intercept conditional recipes
         if(json != null && json.has("type") && json.get("type").isJsonPrimitive() && json.getAsJsonPrimitive("type").isString()){
             String type = json.get("type").getAsString();
             if(RegistryUtil.isValidIdentifier(type) && new ResourceLocation(type).equals(Registries.RECIPE_SERIALIZERS.getIdentifier(ConditionalRecipeSerializer.INSTANCE)))
-                ci.setReturnValue(ConditionalRecipeSerializer.fromJson(recipeLocation, json));
+                ci.setReturnValue(Optional.ofNullable(ConditionalRecipeSerializer.fromJson(recipeLocation, json)));
         }
     }
 }
