9b388c168428e6379d55316fe041f49dcb23f01b
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-02-10 00:19:28 +0100

    Add custom tag entries

--- src/main/java/com/supermartijn642/core/CoreLib.java
+++ src/main/java/com/supermartijn642/core/CoreLib.java
@@ -2,6 +2,8 @@ package com.supermartijn642.core;
 
 import com.supermartijn642.core.data.condition.*;
 import com.supermartijn642.core.data.recipe.ConditionalRecipeSerializer;
+import com.supermartijn642.core.data.tag.CustomTagEntryLoader;
+import com.supermartijn642.core.data.tag.entries.NamespaceTagEntry;
 import com.supermartijn642.core.generator.standard.CoreLibAccessWidenerGenerator;
 import com.supermartijn642.core.generator.standard.CoreLibMiningTagGenerator;
 import com.supermartijn642.core.item.BaseBlockItem;
@@ -27,6 +29,7 @@ public class CoreLib implements ModInitializer {
     public void onInitialize(){
         isArchitecturyLoaded = CommonUtils.isModLoaded("architectury");
         CommonUtils.initialize();
+        CustomTagEntryLoader.init();
 
         // Register conditional recipe type
         RegistrationHandler handler = RegistrationHandler.get("supermartijn642corelib");
@@ -36,6 +39,9 @@ public class CoreLib implements ModInitializer {
         handler.registerResourceConditionSerializer("or", OrResourceCondition.SERIALIZER);
         handler.registerResourceConditionSerializer("and", AndResourceCondition.SERIALIZER);
         handler.registerResourceConditionSerializer("tag_populated", TagPopulatedResourceCondition.SERIALIZER);
+
+        // Register custom tag entry types
+        handler.registerCustomTagEntrySerializer("namespace", NamespaceTagEntry.SERIALIZER);
 
         // Register generator for default tags
         GeneratorRegistrationHandler.get("supermartijn642corelib").addGenerator(cache -> new CoreLibMiningTagGenerator("supermartijn642corelib", cache));
