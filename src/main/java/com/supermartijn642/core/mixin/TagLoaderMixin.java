7f1b2af84a94d57e6e446caca9607fe71f38f351
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-02-10 00:19:28 +0100

    Add custom tag entries

---
+++ src/main/java/com/supermartijn642/core/mixin/TagLoaderMixin.java
@@ -0,0 +1,47 @@
+package com.supermartijn642.core.mixin;
+
+import com.supermartijn642.core.data.tag.TagEntryAdapter;
+import com.supermartijn642.core.extensions.TagLoaderExtension;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.Tag;
+import net.minecraft.tags.TagLoader;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Map;
+
+/**
+ * Created 09/02/2024 by SuperMartijn642
+ */
+@Mixin(TagLoader.class)
+public class TagLoaderMixin implements TagLoaderExtension {
+
+    @Unique
+    private Registry<?> registry;
+
+    @Override
+    public void supermartijn642corelibSetRegistry(Registry<?> registry){
+        this.registry = registry;
+    }
+
+    @Inject(
+        method = "build(Ljava/util/Map;)Ljava/util/Map;",
+        at = @At("HEAD")
+    )
+    private void build(Map<ResourceLocation,Tag.Builder> builders, CallbackInfoReturnable<Map<?,?>> ci){
+        for(Tag.Builder builder : builders.values()){
+            for(Tag.BuilderEntry entry : builder.entries){
+                if(entry.entry() instanceof TagEntryAdapter)
+                    ((TagEntryAdapter)entry.entry()).setRegistry(this.registry);
+            }
+            builder.getRemoveEntries().forEach(entry -> {
+                if(entry.entry() instanceof TagEntryAdapter)
+                    ((TagEntryAdapter)entry.entry()).setRegistry(this.registry);
+            });
+        }
+    }
+}
