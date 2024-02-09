203085186ec7e8cb42759b386673731c0d39a098
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-02-10 00:19:28 +0100

    Add custom tag entries

---
+++ src/main/java/com/supermartijn642/core/data/tag/TagEntryAdapter.java
@@ -0,0 +1,72 @@
+package com.supermartijn642.core.data.tag;
+
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagEntry;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+/**
+ * Created 09/02/2024 by SuperMartijn642
+ */
+public class TagEntryAdapter extends TagEntry {
+
+    final ResourceLocation identifier;
+    final CustomTagEntry customEntry;
+    private Registry<?> registry;
+
+    TagEntryAdapter(ResourceLocation identifier, CustomTagEntry customEntry){
+        super(new ResourceLocation("supermartijn642corelib", "dummy"), false, false);
+        this.identifier = identifier;
+        this.customEntry = customEntry;
+    }
+
+    public void setRegistry(Registry<?> registry){
+        this.registry = registry;
+    }
+
+    @Override
+    public <T> boolean build(Lookup<T> lookup, Consumer<T> entryConsumer){
+        CustomTagEntry.TagEntryResolutionContext<T> context = new CustomTagEntry.TagEntryResolutionContext<T>() {
+            @Override
+            public T getElement(ResourceLocation identifier){
+                return lookup.element(identifier);
+            }
+
+            @Override
+            public Collection<T> getTag(ResourceLocation identifier){
+                return lookup.tag(identifier);
+            }
+
+            @Override
+            public Collection<T> getAllElements(){
+                //noinspection unchecked
+                return (Collection<T>)TagEntryAdapter.this.registry.holders().collect(Collectors.toList());
+            }
+
+            @Override
+            public Set<ResourceLocation> getAllIdentifiers(){
+                return TagEntryAdapter.this.registry.keySet();
+            }
+        };
+        Collection<T> entries = this.customEntry.resolve(context);
+        if(entries != null)
+            entries.forEach(entryConsumer);
+        return true;
+    }
+
+    @Override
+    public void visitOptionalDependencies(Consumer<ResourceLocation> consumer){
+        Collection<ResourceLocation> dependencies = this.customEntry.getTagDependencies();
+        if(dependencies != null)
+            dependencies.forEach(consumer);
+    }
+
+    @Override
+    public String toString(){
+        return "'" + this.identifier + "'{" + this.customEntry + "}";
+    }
+}
