67ac77019f70c57ef69e4719cbe6921d07dd9ecf
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-02-10 00:19:28 +0100

    Add custom tag entries

---
+++ src/main/java/com/supermartijn642/core/extensions/TagLoaderExtension.java
@@ -0,0 +1,11 @@
+package com.supermartijn642.core.extensions;
+
+import net.minecraft.core.Registry;
+
+/**
+ * Created 09/02/2024 by SuperMartijn642
+ */
+public interface TagLoaderExtension {
+
+    void supermartijn642corelibSetRegistry(Registry<?> registry);
+}
