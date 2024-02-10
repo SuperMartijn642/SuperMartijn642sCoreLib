fd3bbdac08dca64c5505345c1addca32a9843d5c
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-02-10 19:43:46 +0100

    Allow custom tag entries to be used in `TagGenerator`

--- src/main/java/com/supermartijn642/core/data/tag/CustomTagEntry.java
+++ src/main/java/com/supermartijn642/core/data/tag/CustomTagEntry.java
@@ -1,6 +1,7 @@
 package com.supermartijn642.core.data.tag;
 
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagEntry;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -10,6 +11,10 @@ import java.util.Set;
  * Created 09/02/2024 by SuperMartijn642
  */
 public interface CustomTagEntry {
+
+    static TagEntry createVanillaEntry(CustomTagEntry customEntry){
+        return CustomTagEntries.wrap(customEntry);
+    }
 
     <T> Collection<T> resolve(TagEntryResolutionContext<T> context);
 
