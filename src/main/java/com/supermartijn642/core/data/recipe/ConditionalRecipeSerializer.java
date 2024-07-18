ddc2bba334717d672e0f61959a8924a572d3f641
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-07-18 02:13:40 +0200

    Fix conditional recipe handling

--- src/main/java/com/supermartijn642/core/data/recipe/ConditionalRecipeSerializer.java
+++ src/main/java/com/supermartijn642/core/data/recipe/ConditionalRecipeSerializer.java
@@ -16,7 +16,10 @@ import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.*;
+import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeInput;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.Level;
 
 import java.util.Collection;
@@ -31,7 +34,7 @@ public final class ConditionalRecipeSerializer implements RecipeSerializer<Recip
             return "supermartijn642corelib:dummy";
         }
     });
-    private static final DummyRecipe DUMMY_RECIPE = new DummyRecipe();
+    public static final Recipe<?> DUMMY_RECIPE = new DummyRecipe();
     public static final ConditionalRecipeSerializer INSTANCE = new ConditionalRecipeSerializer();
 
     public static JsonObject wrapRecipe(JsonObject recipe, Collection<ResourceCondition> conditions){
@@ -53,7 +56,7 @@ public final class ConditionalRecipeSerializer implements RecipeSerializer<Recip
     private ConditionalRecipeSerializer(){
     }
 
-    public static RecipeHolder<?> fromJson(ResourceLocation location, JsonObject json, HolderLookup.Provider provider){
+    public static JsonElement unwrapRecipe(ResourceLocation location, JsonObject json){
         if(!json.has("conditions") || !json.get("conditions").isJsonArray())
             throw new RuntimeException("Conditional recipe '" + location + "' must have 'conditions' array!");
         if(!json.has("recipe") || !json.get("recipe").isJsonObject())
@@ -83,11 +86,11 @@ public final class ConditionalRecipeSerializer implements RecipeSerializer<Recip
             }
 
             if(!condition.test(ResourceConditionContext.EMPTY))
-                return new RecipeHolder<>(location, DUMMY_RECIPE);
+                return null;
         }
 
         // Now return the recipe
-        return RecipeManager.fromJson(location, json.getAsJsonObject("recipe"), provider);
+        return json.getAsJsonObject("recipe");
     }
 
     @Override
