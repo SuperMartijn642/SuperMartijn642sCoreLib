665dc8e85619d13acf33d85a87f8dcaef80c830e
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-08-15 21:43:26 +0200

    Fix registry overrides

---
+++ src/main/java/com/supermartijn642/core/extensions/CoreLibHolderReference.java
@@ -0,0 +1,11 @@
+package com.supermartijn642.core.extensions;
+
+import net.minecraft.resources.ResourceKey;
+
+/**
+ * Created 22/03/2023 by SuperMartijn642
+ */
+public interface CoreLibHolderReference {
+
+    void supermartijn642corelibOverride(ResourceKey<?> key, Object value);
+}
