be2b0c16fa070f3127aec9322882fb15c4079b1a
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-08-27 18:28:55 +0200

    Add `CodecHelper`

--- src/main/java/com/supermartijn642/core/data/tag/CustomTagEntries.java
+++ src/main/java/com/supermartijn642/core/data/tag/CustomTagEntries.java
@@ -3,8 +3,8 @@ package com.supermartijn642.core.data.tag;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import com.mojang.datafixers.util.Either;
-import com.mojang.datafixers.util.Pair;
-import com.mojang.serialization.*;
+import com.mojang.serialization.Codec;
+import com.supermartijn642.core.codec.CodecHelper;
 import com.supermartijn642.core.registry.Registries;
 import com.supermartijn642.core.registry.RegistryUtil;
 import net.minecraft.resources.ResourceLocation;
@@ -18,56 +18,44 @@ import java.util.function.Function;
 public class CustomTagEntries {
 
     public static void init(){
-        TagEntry.CODEC = Codec.either(Codec.<TagEntry>of(
-                new Encoder<>() {
-                    @Override
-                    public <T> DataResult<T> encode(TagEntry input, DynamicOps<T> ops, T prefix){
-                        if(!(input instanceof TagEntryAdapter))
-                            return DataResult.error(() -> "Tag entry is not a custom entry!");
-
-                        JsonObject json = new JsonObject();
-                        CustomTagEntrySerializer<?> serializer = ((TagEntryAdapter)input).customEntry.getSerializer();
-                        json.addProperty("type", Registries.CUSTOM_TAG_ENTRY_SERIALIZERS.getIdentifier(serializer).toString());
-                        try{
-                            //noinspection unchecked,rawtypes
-                            ((CustomTagEntrySerializer)serializer).serialize(json, ((TagEntryAdapter)input).customEntry);
-                        }catch(Exception e){
-                            throw new RuntimeException("Encountered an exception whilst serializing custom tag entry for type '" + Registries.CUSTOM_TAG_ENTRY_SERIALIZERS.getIdentifier(serializer).toString() + "'!");
-                        }
-                        //noinspection unchecked
-                        return DataResult.success((T)json);
+        TagEntry.CODEC = Codec.either(CodecHelper.jsonSerializerToCodec(
+                input -> {
+                    JsonObject json = new JsonObject();
+                    CustomTagEntrySerializer<?> serializer = input.customEntry.getSerializer();
+                    json.addProperty("type", Registries.CUSTOM_TAG_ENTRY_SERIALIZERS.getIdentifier(serializer).toString());
+                    try{
+                        //noinspection unchecked,rawtypes
+                        ((CustomTagEntrySerializer)serializer).serialize(json, input.customEntry);
+                    }catch(Exception e){
+                        throw new RuntimeException("Encountered an exception whilst serializing custom tag entry for type '" + Registries.CUSTOM_TAG_ENTRY_SERIALIZERS.getIdentifier(serializer).toString() + "'!");
                     }
+                    return json;
                 },
-                new Decoder<>() {
-                    @Override
-                    public <T> DataResult<Pair<TagEntry,T>> decode(DynamicOps<T> ops, T input){
-                        if(!(input instanceof JsonObject))
-                            return DataResult.error(() -> "Entry must be an object!");
-                        JsonObject json = (JsonObject)input;
-                        if(!json.has("type") || !json.get("type").isJsonPrimitive() || !json.getAsJsonPrimitive("type").isString())
-                            return DataResult.error(() -> "Missing string key 'type'!");
-                        String typeString = json.get("type").getAsString();
-                        if(!RegistryUtil.isValidIdentifier(typeString))
-                            return DataResult.error(() -> "Invalid identifier '" + typeString + "'!");
-                        ResourceLocation type = new ResourceLocation(typeString);
-                        if(!Registries.CUSTOM_TAG_ENTRY_SERIALIZERS.hasIdentifier(type))
-                            return DataResult.error(() -> "Unknown custom tag entry serializer '" + typeString + "'!");
-                        CustomTagEntrySerializer<?> serializer = Registries.CUSTOM_TAG_ENTRY_SERIALIZERS.getValue(type);
-                        CustomTagEntry customEntry;
-                        try{
-                            customEntry = serializer.deserialize(json);
-                        }catch(JsonParseException e){
-                            return DataResult.error(e::getMessage);
-                        }catch(Exception e){
-                            throw new RuntimeException("Encountered an exception whilst deserializing custom tag entry for type '" + type + "'!", e);
-                        }
-                        return DataResult.success(Pair.of(new TagEntryAdapter(type, customEntry), input));
+                element -> {
+                    if(!(element instanceof JsonObject))
+                        throw new JsonParseException("Entry must be an object!");
+                    JsonObject json = (JsonObject)element;
+                    if(!json.has("type") || !json.get("type").isJsonPrimitive() || !json.getAsJsonPrimitive("type").isString())
+                        throw new JsonParseException("Missing string key 'type'!");
+                    String typeString = json.get("type").getAsString();
+                    if(!RegistryUtil.isValidIdentifier(typeString))
+                        throw new JsonParseException("Invalid identifier '" + typeString + "'!");
+                    ResourceLocation type = new ResourceLocation(typeString);
+                    if(!Registries.CUSTOM_TAG_ENTRY_SERIALIZERS.hasIdentifier(type))
+                        throw new JsonParseException("Unknown custom tag entry serializer '" + typeString + "'!");
+                    CustomTagEntrySerializer<?> serializer = Registries.CUSTOM_TAG_ENTRY_SERIALIZERS.getValue(type);
+                    CustomTagEntry customEntry;
+                    try{
+                        customEntry = serializer.deserialize(json);
+                    }catch(JsonParseException e){
+                        throw new JsonParseException("Encountered an exception whilst deserializing custom tag entry for type '" + type + "'!", e);
                     }
+                    return new TagEntryAdapter(type, customEntry);
                 }
             ), TagEntry.CODEC)
             .xmap(
                 either -> either.map(Function.identity(), Function.identity()),
-                tagEntry -> tagEntry instanceof TagEntryAdapter ? Either.left(tagEntry) : Either.right(tagEntry)
+                tagEntry -> tagEntry instanceof TagEntryAdapter ? Either.left((TagEntryAdapter)tagEntry) : Either.right(tagEntry)
             );
     }
 
