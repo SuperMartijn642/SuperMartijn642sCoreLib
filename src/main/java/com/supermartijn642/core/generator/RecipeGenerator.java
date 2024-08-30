8948f785181bead3e23cf418903bcbe057b594aa
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-08-30 12:59:59 +0200

    Fix conditions not being applied in RecipeGenerator

--- src/main/java/com/supermartijn642/core/generator/RecipeGenerator.java
+++ src/main/java/com/supermartijn642/core/generator/RecipeGenerator.java
@@ -189,7 +189,7 @@ public abstract class RecipeGenerator extends ResourceGenerator {
 
                 // Conditions
                 if(!recipeBuilder.conditions.isEmpty())
-                    ConditionalRecipeSerializer.wrapRecipeWithForgeConditions(json, recipeBuilder.conditions);
+                    json = ConditionalRecipeSerializer.wrapRecipeWithForgeConditions(json, recipeBuilder.conditions);
 
                 // Save the object to the cache
                 ResourceLocation identifier = recipeBuilder.identifier;
