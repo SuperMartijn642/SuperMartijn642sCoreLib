103b137247d92fbaf28981e0b0a9c9ca26cb9224
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-12-23 16:16:18 +0100

    Fix codec argument generics in ClientRegistrationHandler

--- src/main/java/com/supermartijn642/core/registry/ClientRegistrationHandler.java
+++ src/main/java/com/supermartijn642/core/registry/ClientRegistrationHandler.java
@@ -374,7 +374,7 @@ public class ClientRegistrationHandler {
     /**
      * Registers the given special model renderer.
      */
-    public void registerSpecialModelRenderer(String identifier, MapCodec<SpecialModelRenderer.Unbaked> codec){
+    public void registerSpecialModelRenderer(String identifier, MapCodec<? extends SpecialModelRenderer.Unbaked> codec){
         SpecialModelRenderers.ID_MAPPER.put(ResourceLocation.fromNamespaceAndPath(this.modid, identifier), codec);
     }
 
@@ -535,7 +535,7 @@ public class ClientRegistrationHandler {
         this.registerBlockModelRenderType(block, RenderType::translucent);
     }
 
-    public void registerItemModelType(String identifier, MapCodec<ItemModel.Unbaked> codec){
+    public void registerItemModelType(String identifier, MapCodec<? extends ItemModel.Unbaked> codec){
         ItemModels.ID_MAPPER.put(ResourceLocation.fromNamespaceAndPath(this.modid, identifier), codec);
     }
 
