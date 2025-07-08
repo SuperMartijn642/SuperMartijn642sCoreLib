6fb8e7db98427ddcaad84ce2fb5aa788903f3e89
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-07-08 03:01:11 +0200

    Allow submitting arbitrary rendering in `GuiGraphicsHelper`

---
+++ src/main/java/com/supermartijn642/core/CoreLibClient.java
@@ -0,0 +1,16 @@
+package com.supermartijn642.core;
+
+import com.supermartijn642.core.gui.ArbitraryPictureInPictureRenderer;
+import com.supermartijn642.core.registry.ClientRegistrationHandler;
+
+/**
+ * Created 18/03/2022 by SuperMartijn642
+ */
+public class CoreLibClient {
+
+    public static void init(){
+        // Register arbitrary picture in picture renderer
+        ClientRegistrationHandler handler = ClientRegistrationHandler.get("supermartijn642corelib");
+        handler.registerPictureInPictureRenderer(ArbitraryPictureInPictureRenderer::new);
+    }
+}
