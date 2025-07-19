48817ddb24c653504f8e0e92badcd13e04035378
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-07-19 11:49:24 +0200

    Render item tooltips without widget offset in `WidgetContainerScreen` https://github.com/SuperMartijn642/TrashCans/issues/53

--- src/main/java/com/supermartijn642/core/gui/WidgetContainerScreen.java
+++ src/main/java/com/supermartijn642/core/gui/WidgetContainerScreen.java
@@ -122,9 +122,9 @@ public class WidgetContainerScreen<T extends Widget, X extends BaseContainer> ex
         // Render the widget's foreground
         this.widget.renderForeground(this.widgetRenderContext, helper, offsetMouseX, offsetMouseY);
 
-        this.renderTooltip(guiGraphics, offsetMouseX, offsetMouseY);
-
         guiGraphics.pose().popMatrix();
+
+        this.renderTooltip(guiGraphics, mouseX, mouseY);
 
         //noinspection UnstableApiUsage
         ForgeEventFactoryClient.onContainerRenderForeground(this, guiGraphics, mouseX, mouseY);
