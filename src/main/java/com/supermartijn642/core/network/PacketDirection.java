b36db93156a234748e07c2a8dbe528309c338a0b
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-12-29 03:54:50 +0100

    Add packet direction restrictions and handling for configuration stage packets

---
+++ src/main/java/com/supermartijn642/core/network/PacketDirection.java
@@ -0,0 +1,11 @@
+package com.supermartijn642.core.network;
+
+/**
+ * Created 26/12/2025 by SuperMartijn642
+ */
+public enum PacketDirection {
+
+    SERVER_TO_CLIENT,
+    CLIENT_TO_SERVER,
+    BOTH_WAYS
+}
