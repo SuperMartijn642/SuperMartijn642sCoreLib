9761b59670352bdb619c599613bdf51a8ac2a085
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-09 02:11:24 +0100

    Apply scissor area for `ArbitraryPictureInPictureRenderer`

--- src/main/java/com/supermartijn642/core/gui/ArbitraryPictureInPictureRenderer.java
+++ src/main/java/com/supermartijn642/core/gui/ArbitraryPictureInPictureRenderer.java
@@ -153,6 +153,7 @@ public class ArbitraryPictureInPictureRenderer extends PictureInPictureRenderer<
 
     public record State(int x, int y, int width, int height,
                         Matrix3x2f pose,
+                        ScreenRectangle scissorArea,
                         BiConsumer<PoseStack,MultiBufferSource.BufferSource> rendering) implements PictureInPictureRenderState {
 
         @Override
@@ -182,7 +183,7 @@ public class ArbitraryPictureInPictureRenderer extends PictureInPictureRenderer<
 
         @Override
         public ScreenRectangle scissorArea(){
-            return null;
+            return this.scissorArea;
         }
 
         @Override
