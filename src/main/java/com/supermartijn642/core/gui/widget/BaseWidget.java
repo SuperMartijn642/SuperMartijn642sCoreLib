e5f00d9a4e4cc75b0bd87063c43aadd2767de459
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-09 01:03:39 +0100

    When `BaseWidget` is unfocused, also unfocus child widgets

--- src/main/java/com/supermartijn642/core/gui/widget/BaseWidget.java
+++ src/main/java/com/supermartijn642/core/gui/widget/BaseWidget.java
@@ -58,6 +58,8 @@ public abstract class BaseWidget implements Widget {
         if(this.focused != focused)
             this.nextNarration = focused ? System.currentTimeMillis() + 750 : Long.MAX_VALUE;
         this.focused = focused;
+        if(!focused)
+            this.widgets.forEach(w -> w.setFocused(false));
     }
 
     public boolean isFocused(){
