32db17c9006b5b155975532c978fc3289cb180a0
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-10 03:44:55 +0100

    Add `Widget#cursor` to update the mouse cursor

---
+++ src/main/java/com/supermartijn642/core/gui/CursorTypes.java
@@ -0,0 +1,66 @@
+package com.supermartijn642.core.gui;
+
+
+import com.supermartijn642.core.ClientUtils;
+import org.jetbrains.annotations.ApiStatus;
+import org.lwjgl.glfw.GLFW;
+
+/**
+ * Created 10/01/2026 by SuperMartijn642
+ */
+public class CursorTypes {
+
+    static CursorType pendingCursor;
+    private static long lastCursorHandle;
+
+    @ApiStatus.Internal
+    public static void applyPending(){
+        long handle = pendingCursor == null ? 0 : pendingCursor.getCursorHandle();
+        if(handle != lastCursorHandle){
+            GLFW.glfwSetCursor(ClientUtils.getMinecraft().getWindow().getWindow(), handle);
+            lastCursorHandle = handle;
+        }
+        pendingCursor = null;
+    }
+
+    private static final CursorType ARROW = new CursorType(GLFW.GLFW_ARROW_CURSOR);
+    private static final CursorType IBEAM = new CursorType(GLFW.GLFW_IBEAM_CURSOR);
+    private static final CursorType CROSSHAIR = new CursorType(GLFW.GLFW_CROSSHAIR_CURSOR);
+    private static final CursorType POINTING_HAND = new CursorType(GLFW.GLFW_POINTING_HAND_CURSOR);
+    private static final CursorType RESIZE_VERTICAL = new CursorType(GLFW.GLFW_RESIZE_NS_CURSOR);
+    private static final CursorType RESIZE_HORIZONTAL = new CursorType(GLFW.GLFW_RESIZE_EW_CURSOR);
+    private static final CursorType RESIZE_ALL = new CursorType(GLFW.GLFW_RESIZE_ALL_CURSOR);
+    private static final CursorType NOT_ALLOWED = new CursorType(GLFW.GLFW_NOT_ALLOWED_CURSOR);
+
+    public static CursorType arrow(){
+        return ARROW;
+    }
+
+    public static CursorType iBeam(){
+        return IBEAM;
+    }
+
+    public static CursorType crosshair(){
+        return CROSSHAIR;
+    }
+
+    public static CursorType pointingHand(){
+        return POINTING_HAND;
+    }
+
+    public static CursorType resizeVertical(){
+        return RESIZE_VERTICAL;
+    }
+
+    public static CursorType resizeHorizontal(){
+        return RESIZE_HORIZONTAL;
+    }
+
+    public static CursorType resizeAll(){
+        return RESIZE_ALL;
+    }
+
+    public static CursorType notAllowed(){
+        return NOT_ALLOWED;
+    }
+}
