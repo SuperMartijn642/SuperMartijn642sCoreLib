fe5f043cfd9b2cce620a0b126c0708575924ee43
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-10 03:44:55 +0100

    Add `Widget#cursor` to update the mouse cursor

---
+++ src/main/java/com/supermartijn642/core/mixin/GameRendererMixin.java
@@ -0,0 +1,28 @@
+package com.supermartijn642.core.mixin;
+
+import com.supermartijn642.core.gui.CursorTypes;
+import net.minecraft.client.DeltaTracker;
+import net.minecraft.client.renderer.GameRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * Created 15/01/2026 by SuperMartijn642
+ */
+@Mixin(GameRenderer.class)
+public class GameRendererMixin {
+
+    @Inject(
+        method = "render",
+        at = @At(
+            value = "INVOKE",
+            target = "Lcom/mojang/blaze3d/resource/CrossFrameResourcePool;endFrame()V",
+            shift = At.Shift.BEFORE
+        )
+    )
+    private void changeCursor(DeltaTracker deltaTracker, boolean bl, CallbackInfo ci){
+        CursorTypes.applyPending();
+    }
+}
