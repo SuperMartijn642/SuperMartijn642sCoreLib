3b36df829019f8bdc69587595fab419f961e4c5f
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-10 03:44:55 +0100

    Add `Widget#cursor` to update the mouse cursor

--- src/main/java/com/supermartijn642/core/gui/widget/premade/ScrollbarWidget.java
+++ src/main/java/com/supermartijn642/core/gui/widget/premade/ScrollbarWidget.java
@@ -1,6 +1,8 @@
 package com.supermartijn642.core.gui.widget.premade;
 
 import com.supermartijn642.core.TextComponents;
+import com.supermartijn642.core.gui.CursorType;
+import com.supermartijn642.core.gui.CursorTypes;
 import com.supermartijn642.core.gui.ScreenUtils;
 import com.supermartijn642.core.gui.widget.BaseWidget;
 import com.supermartijn642.core.gui.widget.WidgetRenderContext;
@@ -81,8 +83,10 @@ public class ScrollbarWidget extends BaseWidget {
         if(this.dragging){
             if(!this.canUserMoveScroller())
                 this.dragging = false;
-            else
+            else{
                 this.updateDrag(mouseY);
+                ScreenUtils.requestCursor(CursorTypes.resizeVertical());
+            }
         }
 
         super.renderBackground(context, mouseX, mouseY);
@@ -136,6 +140,13 @@ public class ScrollbarWidget extends BaseWidget {
 
     private void updateDrag(int mouseY){
         this.tryScrollTo((mouseY - this.y - this.scrollerHeight / 2f) / (this.height - this.scrollerHeight), false);
+    }
+
+    @Override
+    public CursorType curser(int mouseX, int mouseY){
+        if(!this.active || !this.canUserMoveScroller())
+            return null;
+        return this.dragging ? CursorTypes.resizeVertical() : CursorTypes.pointingHand();
     }
 
     @Override
