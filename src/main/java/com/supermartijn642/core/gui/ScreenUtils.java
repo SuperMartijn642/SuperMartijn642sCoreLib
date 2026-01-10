817785a3c0f762d5fb1d163b6f3e62e829fba14e
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-10 03:44:55 +0100

    Add `Widget#cursor` to update the mouse cursor

--- src/main/java/com/supermartijn642/core/gui/ScreenUtils.java
+++ src/main/java/com/supermartijn642/core/gui/ScreenUtils.java
@@ -364,4 +364,11 @@ public class ScreenUtils {
             RenderSystem.disableScissor();
         }
     }
+
+    /**
+     * @see CursorTypes
+     */
+    public static void requestCursor(CursorType cursorType){
+        CursorTypes.pendingCursor = cursorType;
+    }
 }
