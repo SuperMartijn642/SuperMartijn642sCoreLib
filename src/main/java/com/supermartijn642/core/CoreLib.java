bee39bba5e5723a05e859948411f6809c03602a3
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-10 03:13:16 +0100

    Add `ScrollBarWidget`

--- src/main/java/com/supermartijn642/core/CoreLib.java
+++ src/main/java/com/supermartijn642/core/CoreLib.java
@@ -6,6 +6,7 @@ import com.supermartijn642.core.data.recipe.ConditionalRecipeSerializer;
 import com.supermartijn642.core.data.tag.CustomTagEntries;
 import com.supermartijn642.core.data.tag.entries.NamespaceTagEntry;
 import com.supermartijn642.core.generator.standard.CoreLibAccessWidenerGenerator;
+import com.supermartijn642.core.generator.standard.CoreLibLanguageGenerator;
 import com.supermartijn642.core.generator.standard.CoreLibMiningTagGenerator;
 import com.supermartijn642.core.item.BaseBlockItem;
 import com.supermartijn642.core.item.BaseItem;
@@ -51,6 +52,8 @@ public class CoreLib implements ModInitializer {
         GeneratorRegistrationHandler.get("supermartijn642corelib").addGenerator(cache -> new CoreLibMiningTagGenerator("supermartijn642corelib", cache));
         // Register generator for access widener entries
         GeneratorRegistrationHandler.get("supermartijn642corelib").addGenerator(cache -> new CoreLibAccessWidenerGenerator("supermartijn642corelib", cache));
+        // Register generator for translations
+        GeneratorRegistrationHandler.get("supermartijn642corelib").addGenerator(cache -> new CoreLibLanguageGenerator("supermartijn642corelib", cache));
 
         // Add all BaseItem instances to their respective creative tabs
         ItemGroupEvents.MODIFY_ENTRIES_ALL.register(((group, entries) -> {
