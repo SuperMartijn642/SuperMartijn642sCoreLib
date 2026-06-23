646a6a880b2693fbdab10d329f8138b9f08c57b7
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-06-24 00:19:08 +0200

    Merge `GeneratorRegistrationHandler` entrypoint with existing datagen entrypoints so there's at most one per mod

--- src/main/java/com/supermartijn642/core/mixin/FabricDataGenHelperMixin.java
+++ src/main/java/com/supermartijn642/core/mixin/FabricDataGenHelperMixin.java
@@ -5,10 +5,14 @@ import com.supermartijn642.core.extensions.CoreLibDataGenerator;
 import com.supermartijn642.core.generator.ResourceGenerator;
 import com.supermartijn642.core.registry.GeneratorRegistrationHandler;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.JsonKeySortOrderCallback;
 import net.fabricmc.fabric.impl.datagen.FabricDataGenHelper;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
 import net.fabricmc.loader.api.entrypoint.EntrypointContainer;
+import net.minecraft.core.RegistrySetBuilder;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -34,30 +38,75 @@ public class FabricDataGenHelperMixin {
         GeneratorRegistrationHandler.getAllHandlers().entrySet()
             .stream()
             .filter(entry -> FabricLoader.getInstance().isModLoaded(entry.getKey()))
-            .map(entry -> {
+            .forEach(entry -> {
                 ModContainer container = FabricLoader.getInstance().getModContainer(entry.getKey()).get();
+
+                // Create function to add providers
                 GeneratorRegistrationHandler registrationHandler = entry.getValue();
-                return new EntrypointContainer<DataGeneratorEntrypoint>() {
+                DataGeneratorEntrypoint providers = dataGenerator -> {
+                    try{
+                        ResourceGenerator.registryAccess = dataGenerator.getRegistries().get();
+                    }catch(InterruptedException | ExecutionException e){
+                        throw new RuntimeException(e);
+                    }
+                    //noinspection DataFlowIssue
+                    ((CoreLibDataGenerator)(Object)dataGenerator).setGeneratorRegistrationHandler(registrationHandler);
+                };
+
+                // Find an existing entrypoint for the mod
+                for(int i = 0; i < newEntryPoints.size(); i++){
+                    EntrypointContainer<DataGeneratorEntrypoint> entrypoint = newEntryPoints.get(i);
+                    if(entrypoint.getProvider() == container){
+                        newEntryPoints.set(i, new EntrypointContainer<>() {
+                            @Override
+                            public DataGeneratorEntrypoint getEntrypoint(){
+                                DataGeneratorEntrypoint original = entrypoint.getEntrypoint();
+                                return new DataGeneratorEntrypoint() {
+                                    @Override
+                                    public void onInitializeDataGenerator(FabricDataGenerator dataGenerator){
+                                        original.onInitializeDataGenerator(dataGenerator);
+                                        providers.onInitializeDataGenerator(dataGenerator);
+                                    }
+
+                                    @Override
+                                    public @Nullable String getEffectiveModId(){
+                                        return original.getEffectiveModId();
+                                    }
+
+                                    @Override
+                                    public void buildRegistry(RegistrySetBuilder registryBuilder){
+                                        original.buildRegistry(registryBuilder);
+                                    }
+
+                                    @Override
+                                    public void addJsonKeySortOrders(JsonKeySortOrderCallback callback){
+                                        original.addJsonKeySortOrders(callback);
+                                    }
+                                };
+                            }
+
+                            @Override
+                            public ModContainer getProvider(){
+                                return container;
+                            }
+                        });
+                        return;
+                    }
+                }
+
+                // If there's no existing entrypoint add a new one
+                newEntryPoints.add(new EntrypointContainer<>() {
                     @Override
                     public DataGeneratorEntrypoint getEntrypoint(){
-                        return dataGenerator -> {
-                            try{
-                                ResourceGenerator.registryAccess = dataGenerator.getRegistries().get();
-                            }catch(InterruptedException | ExecutionException e){
-                                throw new RuntimeException(e);
-                            }
-                            //noinspection DataFlowIssue
-                            ((CoreLibDataGenerator)(Object)dataGenerator).setGeneratorRegistrationHandler(registrationHandler);
-                        };
+                        return providers;
                     }
 
                     @Override
                     public ModContainer getProvider(){
                         return container;
                     }
-                };
-            })
-            .forEach(newEntryPoints::add);
+                });
+            });
 
         // Also just make sure resource packs are available
         ClientUtils.getMinecraft().resourcePackRepository.reload();
