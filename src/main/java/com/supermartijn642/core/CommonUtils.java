728631f914e4cd298e9e6980b94fe00754b5997f
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-06-27 20:21:37 +0200

    Add stupid workaround for CommonUtils containing a method call that returns a client class

--- src/main/java/com/supermartijn642/core/CommonUtils.java
+++ src/main/java/com/supermartijn642/core/CommonUtils.java
@@ -49,7 +49,7 @@ public class CommonUtils {
     public static RegistryAccess getRegistryAccess(){
         MinecraftServer server = getServer();
         if(server == null && getEnvironmentSide().isClient()){
-            Level level = ClientUtils.getWorld();
+            Level level = ClientUtils.getLevelNotAsClientLevel();
             return level == null ? null : level.registryAccess();
         }
         return server == null ? null : server.registryAccess();
