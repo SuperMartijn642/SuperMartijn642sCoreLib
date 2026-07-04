ab4c9f7fbfef6c4ec57a0f426451e4307e26e877
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-07-04 20:53:21 +0200

    Add stupid workaround for `PacketContext` containing a method call that returns a client class https://github.com/SuperMartijn642/ChunkLoaders/issues/174

--- src/main/java/com/supermartijn642/core/ClientUtils.java
+++ src/main/java/com/supermartijn642/core/ClientUtils.java
@@ -61,7 +61,7 @@ public class ClientUtils {
         getMinecraft().setScreen(screen);
     }
 
-    static Level getLevelNotAsClientLevel(){
+    public static Level getLevelNotAsClientLevel(){
         return getWorld();
     }
 }
