32fcbed3b51fb0b1b4c5e3ff1cd5fd987623c464
Mervius <dhagemeier2001@gmail.com>
2026-07-28 06:29:02 -0500

    Fix incorrect item interaction results (#89)
    * Fix incorrect item interaction results.
    
    * Fix `BaseItem$ItemUseResult#pass` using `SUCCESS`
    
    ---------
    
    Co-authored-by: Martijn van den Brand <MartijnvandenBrand@outlook.com>

--- src/main/java/com/supermartijn642/core/item/BaseBlockItem.java
+++ src/main/java/com/supermartijn642/core/item/BaseBlockItem.java
@@ -133,7 +133,7 @@ public class BaseBlockItem extends BlockItem {
     public static class ItemUseResult {
 
         public static ItemUseResult pass(ItemStack stack){
-            return new ItemUseResult(InteractionResult.SUCCESS, stack);
+            return new ItemUseResult(InteractionResult.PASS, stack);
         }
 
         public static ItemUseResult consume(ItemStack stack){
@@ -168,7 +168,7 @@ public class BaseBlockItem extends BlockItem {
     }
 
     public enum InteractionFeedback {
-        PASS(InteractionResult.PASS), CONSUME(InteractionResult.CONSUME), SUCCESS(InteractionResult.SUCCESS);
+        PASS(InteractionResult.PASS), CONSUME(InteractionResult.CONSUME), SUCCESS(InteractionResult.SUCCESS), FAIL(InteractionResult.FAIL), CONSUME_PARTIAL(InteractionResult.CONSUME_PARTIAL);
 
         private final InteractionResult interactionResult;
 
@@ -182,9 +182,11 @@ public class BaseBlockItem extends BlockItem {
                 case SUCCESS:
                     return SUCCESS;
                 case CONSUME:
-                case CONSUME_PARTIAL:
-                case FAIL:
                     return CONSUME;
+                case CONSUME_PARTIAL:
+                    return CONSUME_PARTIAL;
+                case FAIL:
+                    return FAIL;
                 case PASS:
                     return PASS;
             }
