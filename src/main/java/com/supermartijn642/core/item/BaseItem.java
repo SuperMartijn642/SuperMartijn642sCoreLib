c2bcb83ee9fba47d6f01a7f1896fc6a2352a07b4
Mervius <dhagemeier2001@gmail.com>
2026-07-28 13:29:02 +0200

    Fix incorrect item interaction results (#89)
    * Fix incorrect item interaction results.
    
    * Fix `BaseItem$ItemUseResult#pass` using `SUCCESS`
    
    ---------
    
    Co-authored-by: Martijn van den Brand <MartijnvandenBrand@outlook.com>

--- src/main/java/com/supermartijn642/core/item/BaseItem.java
+++ src/main/java/com/supermartijn642/core/item/BaseItem.java
@@ -146,7 +146,7 @@ public class BaseItem extends Item {
     protected static class ItemUseResult {
 
         public static ItemUseResult pass(ItemStack stack){
-            return new ItemUseResult(ActionResultType.SUCCESS, stack);
+            return new ItemUseResult(ActionResultType.PASS, stack);
         }
 
         public static ItemUseResult consume(ItemStack stack){
