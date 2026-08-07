b5bce64a3054f3f4dcf382fe67ea2f34cf562d05
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-08-07 21:32:25 +0200

    Add `#getWidget` for widget screens

--- src/main/java/com/supermartijn642/core/gui/WidgetScreen.java
+++ src/main/java/com/supermartijn642/core/gui/WidgetScreen.java
@@ -38,6 +38,10 @@ public class WidgetScreen<T extends Widget> extends Screen {
         this(widget, false);
     }
 
+    public T getWidget(){
+        return this.widget;
+    }
+
     @Override
     protected void init(){
         if(!this.initialized){
