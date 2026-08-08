15112df4e9d92894885de0e34797833651c76a7d
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-08-08 12:26:07 +0200

    Just add items to the creative groups that they're lister for #90

--- src/main/java/com/supermartijn642/core/CoreLib.java
+++ src/main/java/com/supermartijn642/core/CoreLib.java
@@ -1,5 +1,6 @@
 package com.supermartijn642.core;
 
+import com.google.common.collect.ImmutableMap;
 import com.supermartijn642.core.block.BaseBlock;
 import com.supermartijn642.core.data.condition.*;
 import com.supermartijn642.core.data.recipe.ConditionalRecipeSerializer;
@@ -8,17 +9,19 @@ import com.supermartijn642.core.data.tag.entries.NamespaceTagEntry;
 import com.supermartijn642.core.generator.standard.CoreLibAtlasSourceGenerator;
 import com.supermartijn642.core.generator.standard.CoreLibLanguageGenerator;
 import com.supermartijn642.core.generator.standard.CoreLibMiningTagGenerator;
-import com.supermartijn642.core.item.BaseBlockItem;
-import com.supermartijn642.core.item.BaseItem;
 import com.supermartijn642.core.registry.GeneratorRegistrationHandler;
 import com.supermartijn642.core.registry.RegistrationHandler;
 import com.supermartijn642.core.registry.Registries;
 import com.supermartijn642.core.registry.RegistryEntryAcceptor;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLConstructModEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
+
+import java.util.*;
 
 /**
  * Created 7/7/2020 by SuperMartijn642
@@ -60,14 +63,34 @@ public class CoreLib {
 
         // Add all BaseItem instances to their respective creative tabs
         BuildCreativeModeTabContentsEvent.BUS.addListener(event -> {
-            Registries.ITEMS.getValues().stream()
-                .filter(item -> item instanceof BaseItem || item instanceof BaseBlockItem)
-                .filter(item -> item instanceof BaseItem ? ((BaseItem)item).isInCreativeGroup(event.getTab()) : ((BaseBlockItem)item).isInCreativeGroup(event.getTab()))
-                .forEach(event::accept);
+            finalizeItemsPerCreativeGroup();
+            itemsPerCreativeGroup.getOrDefault(event.getTab(), List.of()).forEach(event::accept);
         });
     }
 
     private void onConstructMod(FMLConstructModEvent e){
         RegistryEntryAcceptor.Handler.gatherAnnotatedFields();
+    }
+
+    private static Map<CreativeModeTab,Collection<Item>> itemsPerCreativeGroup = new HashMap<>();
+    private static boolean creativeGroupsInitialized = false;
+
+    public static void addItemToCreativeGroup(CreativeModeTab group, Item item){
+        if(creativeGroupsInitialized)
+            throw new IllegalStateException("Creative mode tabs have already been initialized!");
+        itemsPerCreativeGroup.computeIfAbsent(group, o -> new HashSet<>()).add(item);
+    }
+
+    private static void finalizeItemsPerCreativeGroup(){
+        if(creativeGroupsInitialized)
+            return;
+        creativeGroupsInitialized = true;
+        ImmutableMap.Builder<CreativeModeTab,Collection<Item>> builder = ImmutableMap.builder();
+        itemsPerCreativeGroup.forEach((group, items) -> {
+            List<Item> sorted = new ArrayList<>(items);
+            sorted.sort(Comparator.comparing(Registries.ITEMS::getIdentifier));
+            builder.put(group, List.copyOf(sorted));
+        });
+        itemsPerCreativeGroup = builder.build();
     }
 }
