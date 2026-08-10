cc7a4284c95afccf6b4db73ffc2d8ef9a023823c
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-08-10 11:28:00 +0200

    Use buffer source from gui graphics for `ScreenUtils` while a screen is being rendered

--- src/main/java/com/supermartijn642/core/mixin/GameRendererMixin.java
+++ src/main/java/com/supermartijn642/core/mixin/GameRendererMixin.java
@@ -1,6 +1,9 @@
 package com.supermartijn642.core.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import com.supermartijn642.core.gui.CursorTypes;
+import com.supermartijn642.core.gui.ScreenUtils;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.renderer.GameRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -23,5 +26,29 @@ public class GameRendererMixin {
     )
     private void changeCursor(float f, long l, boolean bl, CallbackInfo ci){
         CursorTypes.applyPending();
+    }
+
+    @Inject(
+        method = "render",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/gui/screens/Screen;renderWithTooltip(Lnet/minecraft/client/gui/GuiGraphics;IIF)V",
+            shift = At.Shift.BEFORE
+        )
+    )
+    private void beforeDrawScreen(float f, long l, boolean bl, CallbackInfo ci, @Local GuiGraphics guiGraphics) {
+        ScreenUtils.bufferSourceOverwrite = guiGraphics.bufferSource;
+    }
+
+    @Inject(
+        method = "render",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/gui/screens/Screen;renderWithTooltip(Lnet/minecraft/client/gui/GuiGraphics;IIF)V",
+            shift = At.Shift.AFTER
+        )
+    )
+    private void afterDrawScreen(float f, long l, boolean bl, CallbackInfo ci, @Local GuiGraphics guiGraphics) {
+        ScreenUtils.bufferSourceOverwrite = null;
     }
 }
