880c5fe211f4bf340027a4611ec4d100274e2bc3
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-08-10 11:28:00 +0200

    Use buffer source from gui graphics for `ScreenUtils` while a screen is being rendered

--- src/main/java/com/supermartijn642/core/gui/ScreenUtils.java
+++ src/main/java/com/supermartijn642/core/gui/ScreenUtils.java
@@ -43,10 +43,16 @@ public class ScreenUtils {
     private static final ResourceLocation SCREEN_BACKGROUND = ResourceLocation.fromNamespaceAndPath("supermartijn642corelib", "textures/gui/background.png");
     private static final GuiGraphics GUI_GRAPHICS = new GuiGraphics(ClientUtils.getMinecraft(), null);
 
+    public static MultiBufferSource.BufferSource bufferSourceOverwrite = null;
+
+    private static MultiBufferSource.BufferSource getBufferSource(){
+        return bufferSourceOverwrite == null ? RenderUtils.getMainBufferSource() : bufferSourceOverwrite;
+    }
+
     public static final int DEFAULT_TEXT_COLOR = 4210752, ACTIVE_TEXT_COLOR = 14737632, INACTIVE_TEXT_COLOR = 7368816;
 
     public static void drawString(PoseStack poseStack, Font fontRenderer, Component text, float x, float y, int color){
-        MultiBufferSource.BufferSource bufferSource = ClientUtils.getMinecraft().gameRenderer.renderBuffers.bufferSource();
+        MultiBufferSource.BufferSource bufferSource = getBufferSource();
         fontRenderer.drawInBatch(text, x, y, color, false, poseStack.last().pose(), bufferSource, Font.DisplayMode.NORMAL, 0, LightTexture.FULL_BRIGHT);
         bufferSource.endBatch();
     }
@@ -64,7 +70,7 @@ public class ScreenUtils {
     }
 
     public static void drawStringWithShadow(PoseStack poseStack, Font fontRenderer, Component text, float x, float y, int color){
-        MultiBufferSource.BufferSource bufferSource = ClientUtils.getMinecraft().gameRenderer.renderBuffers.bufferSource();
+        MultiBufferSource.BufferSource bufferSource = getBufferSource();
         fontRenderer.drawInBatch(text, x, y, color, true, poseStack.last().pose(), bufferSource, Font.DisplayMode.NORMAL, 0, LightTexture.FULL_BRIGHT);
         bufferSource.endBatch();
     }
@@ -114,7 +120,7 @@ public class ScreenUtils {
     }
 
     public static void drawString(PoseStack poseStack, Font fontRenderer, String text, float x, float y, int color){
-        MultiBufferSource.BufferSource bufferSource = ClientUtils.getMinecraft().gameRenderer.renderBuffers.bufferSource();
+        MultiBufferSource.BufferSource bufferSource = getBufferSource();
         fontRenderer.drawInBatch(text, x, y, color, false, poseStack.last().pose(), bufferSource, Font.DisplayMode.NORMAL, 0, LightTexture.FULL_BRIGHT);
         bufferSource.endBatch();
     }
@@ -132,7 +138,7 @@ public class ScreenUtils {
     }
 
     public static void drawStringWithShadow(PoseStack poseStack, Font fontRenderer, String text, float x, float y, int color){
-        MultiBufferSource.BufferSource bufferSource = ClientUtils.getMinecraft().gameRenderer.renderBuffers.bufferSource();
+        MultiBufferSource.BufferSource bufferSource = getBufferSource();
         fontRenderer.drawInBatch(text, x, y, color, true, poseStack.last().pose(), bufferSource, Font.DisplayMode.NORMAL, 0, LightTexture.FULL_BRIGHT);
         bufferSource.endBatch();
     }
@@ -295,7 +301,7 @@ public class ScreenUtils {
 
         GUI_GRAPHICS.minecraft = ClientUtils.getMinecraft();
         GUI_GRAPHICS.pose = poseStack;
-        GUI_GRAPHICS.bufferSource = ClientUtils.getMinecraft().gameRenderer.renderBuffers.bufferSource();
+        GUI_GRAPHICS.bufferSource = getBufferSource();
         GUI_GRAPHICS.renderTooltipInternal(fontRenderer, components, x, y, DefaultTooltipPositioner.INSTANCE);
         GUI_GRAPHICS.bufferSource.endBatch();
     }
@@ -319,7 +325,7 @@ public class ScreenUtils {
                 Lighting.setupForFlatItems();
             RenderSystem.disableDepthTest();
 
-            MultiBufferSource.BufferSource bufferSource = ClientUtils.getMinecraft().gameRenderer.renderBuffers.bufferSource();;
+            MultiBufferSource.BufferSource bufferSource = getBufferSource();
             ClientUtils.getItemRenderer().render(stack, ItemDisplayContext.GUI, false, poseStack, bufferSource, LightTexture.FULL_BRIGHT, OverlayTexture.NO_OVERLAY, model);
             bufferSource.endBatch();
 
@@ -339,7 +345,8 @@ public class ScreenUtils {
 
     public static void withScissor(PoseStack poseStack, int x, int y, int width, int height, Runnable rendering){
         // Draw current buffers before enabling scissor
-        RenderUtils.getMainBufferSource().endBatch();
+        MultiBufferSource.BufferSource bufferSource = getBufferSource();
+        bufferSource.endBatch();
 
         // Apply matrix stack to given coordinates
         Vector3f scissorStart = poseStack.last().pose().transformPosition(x, y, 0, new Vector3f());
@@ -355,7 +362,7 @@ public class ScreenUtils {
         RenderSystem.enableScissor((int)scissorX, (int)scissorY, Math.max(0, (int)scissorWidth), Math.max(0, (int)scissorHeight));
         try{
             rendering.run();
-            RenderUtils.getMainBufferSource().endBatch();
+            bufferSource.endBatch();
         }finally{
             RenderSystem.disableScissor();
         }
