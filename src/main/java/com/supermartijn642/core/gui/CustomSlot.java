4f0af5c3e8ee0ee7fc0f8028b46f09ebae230dbc
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-08-17 15:55:58 +0200

    Separate vanilla `Slot` implementation from `CustomSlot` implementation to prevent ForgeGradle from obfuscating `CustomSlot` methods https://github.com/SuperMartijn642/TrashCans/issues/56

--- src/main/java/com/supermartijn642/core/gui/CustomSlot.java
+++ src/main/java/com/supermartijn642/core/gui/CustomSlot.java
@@ -7,12 +7,14 @@ import net.minecraft.world.Container;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.ApiStatus;
 
 import java.util.function.*;
 
 /**
  * Created 29/01/2023 by SuperMartijn642
  */
+@ApiStatus.NonExtendable
 public interface CustomSlot {
 
     static Builder builder(){
