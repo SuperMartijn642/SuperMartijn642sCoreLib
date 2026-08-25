5d512584b2ec85fad6a685dc68ab86f211f1086c
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-08-26 01:00:55 +0200

    Use modloader context to check for tags in `TagPopulatedResourceCondition`

--- src/main/java/com/supermartijn642/core/data/condition/TagPopulatedResourceCondition.java
+++ src/main/java/com/supermartijn642/core/data/condition/TagPopulatedResourceCondition.java
@@ -3,9 +3,7 @@ package com.supermartijn642.core.data.condition;
 import com.google.gson.JsonObject;
 import com.supermartijn642.core.registry.Registries;
 import com.supermartijn642.core.registry.RegistryUtil;
-import net.minecraft.core.Registry;
 import net.minecraft.resources.Identifier;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.tags.TagKey;
 
 /**
@@ -29,9 +27,7 @@ public class TagPopulatedResourceCondition implements ResourceCondition {
 
     @Override
     public boolean test(ResourceConditionContext context){
-        ResourceKey<?> registryKey = this.registry.hasForgeRegistry() ? this.registry.getForgeRegistry().getRegistryKey() : this.registry.getVanillaRegistry().key();
-        //noinspection unchecked
-        return !context.getUnderlying().getTag(TagKey.create((ResourceKey<? extends Registry<Object>>)registryKey, this.tag)).isEmpty();
+        return context.isTagPopulated(TagKey.create(this.registry.getVanillaRegistry().key(), this.tag));
     }
 
     @Override
