d8388c0a1c86a43f6537f6008b982833a35d0425
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-08-26 01:00:55 +0200

    Use modloader context to check for tags in `TagPopulatedResourceCondition`

--- src/main/java/com/supermartijn642/core/data/condition/ResourceConditionContext.java
+++ src/main/java/com/supermartijn642/core/data/condition/ResourceConditionContext.java
@@ -1,5 +1,6 @@
 package com.supermartijn642.core.data.condition;
 
+import net.minecraft.tags.TagKey;
 import net.neoforged.neoforge.common.conditions.ICondition;
 
 /**
@@ -13,6 +14,14 @@ public class ResourceConditionContext {
 
     ResourceConditionContext(ICondition.IContext context){
         this.context = context;
+    }
+
+    public boolean isTagAvailable(TagKey<?> tag){
+        return this.context.isTagLoaded(tag);
+    }
+
+    public boolean isTagPopulated(TagKey<?> tag){
+        return !this.context.getTag(tag).isEmpty();
     }
 
     @Deprecated
